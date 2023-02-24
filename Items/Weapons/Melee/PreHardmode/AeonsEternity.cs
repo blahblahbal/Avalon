964c759748282b6b0479d9496e96a653055f8547
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-02-24 14:04:03 -0600

    recipe

--- Items/Weapons/Melee/PreHardmode/AeonsEternity.cs
+++ Items/Weapons/Melee/PreHardmode/AeonsEternity.cs
@@ -116,6 +116,40 @@ namespace ExxoAvalonOrigins.Items.Weapons.Melee.PreHardmode
                 .AddIngredient(ModContent.ItemType<OsmiumGreatsword>())
                 .AddTile(TileID.DemonAltar)
                 .Register();
+
+            CreateRecipe(1)
+                .AddIngredient(ItemID.Starfury)
+                .AddIngredient(ItemID.IceBlade)
+                .AddIngredient(ModContent.ItemType<MinersSword>())
+                .AddIngredient(ItemID.AntlionClaw)
+                .AddIngredient(ModContent.ItemType<RhodiumGreatsword>())
+                .AddTile(TileID.DemonAltar)
+                .Register();
+            CreateRecipe(1)
+                .AddIngredient(ItemID.Starfury)
+                .AddIngredient(ItemID.IceBlade)
+                .AddIngredient(ModContent.ItemType<MinersSword>())
+                .AddIngredient(ModContent.ItemType<DesertLongsword>())
+                .AddIngredient(ModContent.ItemType<RhodiumGreatsword>())
+                .AddTile(TileID.DemonAltar)
+                .Register();
+
+            CreateRecipe(1)
+                .AddIngredient(ItemID.Starfury)
+                .AddIngredient(ItemID.IceBlade)
+                .AddIngredient(ModContent.ItemType<MinersSword>())
+                .AddIngredient(ItemID.AntlionClaw)
+                .AddIngredient(ModContent.ItemType<IridiumGreatsword>())
+                .AddTile(TileID.DemonAltar)
+                .Register();
+            CreateRecipe(1)
+                .AddIngredient(ItemID.Starfury)
+                .AddIngredient(ItemID.IceBlade)
+                .AddIngredient(ModContent.ItemType<MinersSword>())
+                .AddIngredient(ModContent.ItemType<DesertLongsword>())
+                .AddIngredient(ModContent.ItemType<IridiumGreatsword>())
+                .AddTile(TileID.DemonAltar)
+                .Register();
         }
     }
 }
