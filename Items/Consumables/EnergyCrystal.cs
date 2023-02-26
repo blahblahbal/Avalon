4a0abc7d6496b0d3955c1667cad139fd66ae66c4
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-02-26 09:13:57 -0600

    Stamina Crystla 2

---
+++ Items/Consumables/EnergyCrystal.cs
@@ -0,0 +1,31 @@
+﻿using ExxoAvalonOrigins.Common;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ExxoAvalonOrigins.Items.Consumables
+{
+    public class EnergyCrystal : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 1;
+        }
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.AegisCrystal);
+            Item.Size = new Vector2(16, 16);
+        }
+        public override bool? UseItem(Player player)
+        {
+            player.GetModPlayer<AvalonStaminaPlayer>().EnergyCrystal = true;
+            return base.UseItem(player);
+        }
+    }
+}
