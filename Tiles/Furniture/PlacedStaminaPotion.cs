6e763187a557f9b0a9369252788d6ec6f2e9ec67
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-04-16 14:23:08 -0500

    Resprites and placable stamina potion

---
+++ Tiles/Furniture/PlacedStaminaPotion.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using Avalon.Items.Potions.Other;
+using Terraria.Audio;
+
+namespace Avalon.Tiles.Furniture
+{
+    public class PlacedStaminaPotion : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+            TileObjectData.addTile(Type);
+            Main.tileLighted[Type] = true;
+            DustType = DustID.Glass;
+            AddMapEntry(Color.Green);
+        }
+        public override bool RightClick(int i, int j)
+        {
+            WorldGen.KillTile(i, j,false,false,true);
+            if (!Main.tile[i, j].HasTile && Main.netMode != NetmodeID.SinglePlayer)
+            {
+                NetMessage.SendData(MessageID.TileManipulation, -1, -1, null, 0, i, j);
+            }
+            return true;
+        }
+        public override void MouseOver(int i, int j)
+        {
+            Player player = Main.LocalPlayer;
+            player.noThrow = 2;
+            player.cursorItemIconEnabled = true;
+            player.cursorItemIconID = ModContent.ItemType<LesserStaminaPotion>();
+        }
+        public override bool KillSound(int i, int j, bool fail)
+        {
+            if (!fail)
+            {
+                SoundEngine.PlaySound(SoundID.Shatter, new Vector2(i, j).ToWorldCoordinates());
+                return false;
+            }
+            return base.KillSound(i, j, fail);
+        }
+
+        public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
+        {
+            noItem = true;
+            Item.NewItem(WorldGen.GetItemSource_FromTileBreak(i, j), i * 16, j * 16, 16, 16, ModContent.ItemType<LesserStaminaPotion>(), pfix: -1);
+        }
+    }
+}
