0630b97d0ddce87cd13b45d6b62480e19d0464a6
blahblahbal <jabawakijadehawk@gmail.com>
2023-04-17 15:42:55 -0700

    Extra Mana
    A few issues with this atm:
    - Classic bars don't work somehow. Looked at the source for 1.4.3 and 1.4.4 and they are identical so I don't know why the IL doesn't work.
    -Some other minor things are weird.

---
+++ Hooks/ExtraMana.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Reflection;
+using Avalon.Common;
+using Microsoft.Xna.Framework.Graphics;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using ReLogic.Content;
+using Terraria;
+using Terraria.GameContent.UI.ResourceSets;
+using Terraria.ModLoader;
+
+namespace Avalon.Hooks;
+
+[Autoload(Side = ModSide.Client)]
+public class ExtraMana : ModHook
+{
+    private const int LowManaTier = 2;
+    private const int ManaPerCrystal = 20;
+    private const int MaxManaCrystalsToDisplay = 10;
+    private const int MaxManaToDisplay = MaxManaCrystalsToDisplay * ManaPerCrystal;
+    private const int TopManaTier = 6;
+
+    private static readonly Func<HorizontalBarsPlayerResourcesDisplaySet, int> GetMPSegmentsCount =
+        Utilities.CreateInstancePropertyOrFieldReaderDelegate<HorizontalBarsPlayerResourcesDisplaySet, int>(
+            "_mpSegmentsCount");
+
+    protected override void Apply()
+    {
+        On_PlayerStatsSnapshot.ctor += OnPlayerStatsSnapshotCtor;
+        On_FancyClassicPlayerResourcesDisplaySet.PrepareFields += OnFancyClassicPrepareFields;
+        //IL_ClassicPlayerResourcesDisplaySet.DrawMana += ILClassicDrawMana;
+        IL_FancyClassicPlayerResourcesDisplaySet.StarFillingDrawer +=
+            ILStarFillingDrawer;
+        IL_HorizontalBarsPlayerResourcesDisplaySet.ManaFillingDrawer +=
+            ILManaFillingDrawer;
+    }
+
+    private static void OnPlayerStatsSnapshotCtor(
+        On_PlayerStatsSnapshot.orig_ctor orig,
+        ref PlayerStatsSnapshot self,
+        Player player)
+    {
+        orig(ref self, player);
+
+        if (self.ManaMax > MaxManaToDisplay)
+        {
+            var field = typeof(PlayerStatsSnapshot).GetField("numManaStars", BindingFlags.Instance | BindingFlags.NonPublic);
+            field.SetValue(self, player.statManaMax2 / MaxManaCrystalsToDisplay);
+        }
+    }
+
+
+    private static int RoundToNearest10(int num)
+    {
+        float original = num / 10f;
+        if (original > 1f && original <= 1.5f)
+        {
+            original = 1f;
+        }
+        else if (original > 1.5f && original < 2f)
+        {
+            original = 2f;
+        }
+        return (int)(original * 10f);
+    }
+
+
+    private static void OnFancyClassicPrepareFields(On_FancyClassicPlayerResourcesDisplaySet.orig_PrepareFields orig,
+        FancyClassicPlayerResourcesDisplaySet self, Player player)
+    {
+        orig(self, player);
+        var snapshot = typeof(FancyClassicPlayerResourcesDisplaySet).GetField("preparedSnapshot", BindingFlags.Instance | BindingFlags.NonPublic);
+        if (player.statManaMax2 > MaxManaToDisplay)
+        {
+            PlayerStatsSnapshot snap = (PlayerStatsSnapshot)snapshot.GetValue(self);
+            var field = typeof(FancyClassicPlayerResourcesDisplaySet).GetField("_starCount", BindingFlags.Instance | BindingFlags.NonPublic);
+            int val = snap.ManaMax / (int)(MaxManaCrystalsToDisplay * (snap.ManaMax / 100));
+            if (val > 10)
+            {
+                val = RoundToNearest10(val);
+            }
+            field.SetValue(self, val);
+            snapshot.SetValue(self, snap);
+        }
+    }
+
+    private static void ILClassicDrawMana(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(i => i.MatchLdcI4(20))
+            .GotoNext(i => i.MatchStfld(out _));
+
+        c.EmitDelegate<Func<int, int>>(val =>
+        {
+            if (Main.LocalPlayer.statManaMax2 > MaxManaToDisplay)
+            {
+                return Main.LocalPlayer.statManaMax2 / MaxManaCrystalsToDisplay;
+            }
+
+            return val;
+        });
+
+        c.GotoNext(i => i.MatchLdcR8(0.9))
+            .GotoNext(i => i.MatchLdsfld(out _))
+            .GotoNext(i => i.MatchCallvirt(out _))
+            .Emit(OpCodes.Ldloc, 6);
+
+        c.EmitDelegate<Func<Asset<Texture2D>, int, Asset<Texture2D>>>((sprite, index) =>
+        {
+            for (int i = TopManaTier; i >= LowManaTier; i--)
+            {
+                if (index - 1 < (Main.LocalPlayer.statManaMax2 - (MaxManaToDisplay * (i - 1))) / ManaPerCrystal)
+                {
+                    return ExxoAvalonOrigins.Mod.Assets.Request<Texture2D>($"{ExxoAvalonOrigins.TextureAssetsPath}/UI/Mana{i}");
+                }
+            }
+
+            return sprite;
+        });
+    }
+
+    private static void ILStarFillingDrawer(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(i => i.MatchLdfld(out _))
+            .GotoNext(i => i.MatchStindRef())
+            .Emit(OpCodes.Ldarg, 1);
+
+        c.EmitDelegate<Func<Asset<Texture2D>, int, Asset<Texture2D>>>((sprite, index) =>
+        {
+            for (int i = TopManaTier; i >= LowManaTier; i--)
+            {
+                if (index < (Main.LocalPlayer.statManaMax2 - (MaxManaToDisplay * (i - 1))) / ManaPerCrystal)
+                {
+                    return ExxoAvalonOrigins.Mod.Assets.Request<Texture2D>(
+                        $"{ExxoAvalonOrigins.TextureAssetsPath}/UI/FancyMana{i}");
+                }
+            }
+
+            return sprite;
+        });
+    }
+
+    private static void ILManaFillingDrawer(ILContext il)
+    {
+        var c = new ILCursor(il);
+
+        c.GotoNext(i => i.MatchLdfld(out _))
+            .GotoNext(i => i.MatchStindRef())
+            .Emit(OpCodes.Ldarg, 1)
+            .Emit(OpCodes.Ldarg_0);
+
+        c.EmitDelegate<Func<Asset<Texture2D>, int, HorizontalBarsPlayerResourcesDisplaySet, Asset<Texture2D>>>(
+            (sprite, index, self) =>
+            {
+                int mpSegmentsCount = GetMPSegmentsCount(self);
+                for (int i = TopManaTier; i >= LowManaTier; i--)
+                {
+                    if (index >= mpSegmentsCount -
+                        ((Main.LocalPlayer.statManaMax2 - (MaxManaToDisplay * (i - 1))) / ManaPerCrystal))
+                    {
+                        return ExxoAvalonOrigins.Mod.Assets.Request<Texture2D>(
+                            $"{ExxoAvalonOrigins.TextureAssetsPath}/UI/BarMana{i}");
+                    }
+                }
+
+                return sprite;
+            });
+    }
+}
---
+++ Hooks/Utilities.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Reflection;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+
+namespace Avalon.Hooks;
+
+public static class Utilities
+{
+    /// <summary>
+    ///     Creates a delegate for reading instance property or field values that is faster than a reflection implementation,
+    ///     this should only be used to create a cached expression as the compilation is expensive.
+    /// </summary>
+    /// <param name="fieldName">The name of the property or field.</param>
+    /// <typeparam name="TInstance">The type of the instance that the field belongs to.</typeparam>
+    /// <typeparam name="TResult">The type of the property or field.</typeparam>
+    /// <returns>A delegate that provides the property or field value when supplied with an instance.</returns>
+    public static Func<TInstance, TResult> CreateInstancePropertyOrFieldReaderDelegate<TInstance, TResult>(
+        string fieldName)
+    {
+        ParameterExpression instanceParameter = Expression.Parameter(typeof(TInstance));
+        return Expression
+            .Lambda<Func<TInstance, TResult>>(
+                Expression.PropertyOrField(instanceParameter, fieldName),
+                instanceParameter).Compile();
+    }
+
+    public static void OutputIL(ILContext il)
+    {
+        var c = new ILCursor(il);
+        foreach (Instruction instruction in c.Instrs)
+        {
+            object obj = (instruction.Operand == null ? string.Empty : instruction.Operand.ToString()) ?? string.Empty;
+            ExxoAvalonOrigins.Mod.Logger.Debug(
+                $"{instruction.Offset.ToString(CultureInfo.InvariantCulture)} | {instruction.OpCode.ToString()} | {obj}");
+        }
+    }
+
+    public static List<Instruction> FromCursorToInstruction(ILCursor c, Func<Instruction, bool> predicate)
+    {
+        List<Instruction> instructions = new();
+        while (!predicate.Invoke(c.Next))
+        {
+            instructions.Add(c.Next);
+            c.Index++;
+        }
+
+        return instructions;
+    }
+
+    public static void EmitInstructions(ILCursor c, IEnumerable<Instruction> instructions)
+    {
+        foreach (Instruction instruction in instructions)
+        {
+            c.Emit(instruction.OpCode, instruction.Operand);
+        }
+    }
+
+    public static void RemoveUntilInstruction(ILCursor c, Func<Instruction, bool> predicate)
+    {
+        while (!predicate.Invoke(c.Next))
+        {
+            c.Remove();
+        }
+    }
+
+    public static void AddAlternativeIdChecks(ILContext il, ushort origId, Func<ushort, bool> predicate)
+    {
+        var c = new ILCursor(il);
+
+        while (c.TryGotoNext(i =>
+                   (i.MatchBeq(out _) || i.MatchBneUn(out _)) && i.Offset != 0 && i.Previous.MatchLdcI4(origId)))
+        {
+            c.Index--;
+            c.EmitDelegate<Func<ushort, ushort>>(id => predicate.Invoke(id) ? origId : id);
+            c.Index += 2;
+        }
+    }
+
+    public static void SoftReplaceAllMatchingInstructionsWithMethod(ILContext il, Instruction i1, MethodBase method)
+    {
+        var c = new ILCursor(il);
+
+        while (c.TryGotoNext(i => i.OpCode == i1.OpCode && (i1.Operand == null || i.Operand == i1.Operand)))
+        {
+            // Ensure not replacing anything added by an IL hook
+            if (c.Next.Offset != 0)
+            {
+                c.Index++;
+                c.Emit(OpCodes.Pop);
+                c.Emit(OpCodes.Call, method);
+            }
+        }
+    }
+}
