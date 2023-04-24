bef5920c945064fe5a85f78413b6fdbe9fa23315
TerrorPenguin <terra_penguin@live.com>
2023-04-24 16:30:18 +1000

    updated pandemite & placed osmium bar
    also fixed bar drops & dusts

--- Tiles/PlacedBars.cs
+++ Tiles/PlacedBars.cs
@@ -36,49 +36,49 @@ public class PlacedBars : ModTile
     }
     public override void KillTile(int i, int j, ref bool fail, ref bool effectOnly, ref bool noItem)
     {
-        if (fail)
-        {
-            return;
-        }
-        int toDrop = 0;
+        //if (fail)
+        //{
+        //    return;
+        //}
+        //int toDrop = 0;
         switch (Main.tile[i, j].TileFrameX / 18)
         {
             //case 0:
             //    toDrop = ModContent.ItemType<CaesiumBar>();
             //    DustType = ModContent.DustType<Dusts.CaesiumDust>();
             //    break;
-            //case 1:
+            case 1:
             //    toDrop = ModContent.ItemType<OblivionBar>();
-            //    DustType = DustID.Adamantite;
-            //    break;
-            //case 2:
-            //    toDrop = ModContent.ItemType<HydrolythBar>();
-            //    DustType = DustID.MagicMirror;
-            //    break;
-            //case 3:
-            //    toDrop = ModContent.ItemType<RhodiumBar>();
-            //    DustType = DustID.t_LivingWood;
-            //    break;
-            case 4:
-                toDrop = ModContent.ItemType<OsmiumBar>();
-                //DustType = ModContent.DustType<Dusts.OsmiumDust>();
+                DustType = DustID.Adamantite;
                 break;
-            //case 5:
+            case 2:
+            //    toDrop = ModContent.ItemType<HydrolythBar>();
+                DustType = DustID.MagicMirror;
+                break;
+            case 3:
+            //    toDrop = ModContent.ItemType<RhodiumBar>();
+                DustType = DustID.t_LivingWood;
+                break;
+            case 4:
+            //    toDrop = ModContent.ItemType<OsmiumBar>();
+                DustType = ModContent.DustType<Dusts.OsmiumDust>();
+                break;
+            case 5:
             //    toDrop = ModContent.ItemType<FeroziumBar>();
-            //    DustType = DustID.UltraBrightTorch;
-            //    break;
-            //case 6:
+                DustType = DustID.UltraBrightTorch;
+                break;
+            case 6:
             //    toDrop = ModContent.ItemType<UnvolanditeBar>();
-            //    DustType = DustID.Dirt;
-            //    break;
-            //case 7:
+                DustType = DustID.Dirt;
+                break;
+            case 7:
             //    toDrop = ModContent.ItemType<VorazylcumBar>();
-            //    DustType = DustID.CorruptGibs;
-            //    break;
-            //case 8:
+                DustType = DustID.CorruptGibs;
+                break;
+            case 8:
             //    toDrop = ModContent.ItemType<TritanoriumBar>();
-            //    DustType = DustID.Stone;
-            //    break;
+                DustType = DustID.Stone;
+                break;
             //case 9:
             //    toDrop = ModContent.ItemType<DurataniumBar>();
             //    DustType = ModContent.DustType<Dusts.DurataniumDust>();
@@ -91,59 +91,59 @@ public class PlacedBars : ModTile
             //    toDrop = ModContent.ItemType<TroxiniumBar>();
             //    DustType = ModContent.DustType<Dusts.TroxiniumDust>();
             //    break;
-            //case 12:
+            case 12:
             //    toDrop = ModContent.ItemType<PandemiteBar>();
-            //    DustType = DustID.JungleSpore;
-            //    break;
-            //case 13:
-            //    toDrop = ModContent.ItemType<PyroscoricBar>();
-            //    DustType = DustID.InfernoFork;
-            //    break;
-            //case 14:
-            //    toDrop = ModContent.ItemType<BeetleBar>();
-            //    DustType = DustID.RainCloud;
-            //    break;
-            //case 15:
-            //    //toDrop = ModContent.ItemType<SuperhardmodeBar>();
-            //    DustType = DustID.CrimtaneWeapons;
-            //    break;
-            case 16:
-                toDrop = ModContent.ItemType<EnchantedBar>();
+                DustType = DustID.JungleSpore;
                 break;
-            //case 17:
+            case 13:
+            //    toDrop = ModContent.ItemType<PyroscoricBar>();
+                DustType = DustID.InfernoFork;
+                break;
+            case 14:
+            //    toDrop = ModContent.ItemType<BeetleBar>();
+                DustType = DustID.RainCloud;
+                break;
+            case 15:
+            //    //toDrop = ModContent.ItemType<SuperhardmodeBar>();
+                DustType = DustID.CrimtaneWeapons;
+                break;
+            //case 16:
+            //    toDrop = ModContent.ItemType<EnchantedBar>();
+            //    break;
+            case 17:
             //    //toDrop = ModContent.ItemType<BerserkerBar>();
-            //    DustType = DustID.Ice_Pink;
-            //    break;
-            //case 18:
+                DustType = DustID.Ice_Pink;
+                break;
+            case 18:
             //    toDrop = ModContent.ItemType<BronzeBar>();
-            //    DustType = ModContent.DustType<Dusts.BronzeDust>();
-            //    break;
-            //case 19:
+                DustType = ModContent.DustType<Dusts.BronzeDust>();
+                break;
+            case 19:
             //    toDrop = ModContent.ItemType<NickelBar>();
-            //    DustType = ModContent.DustType<Dusts.NickelDust>();
-            //    break;
-            //case 20:
+                DustType = ModContent.DustType<Dusts.NickelDust>();
+                break;
+            case 20:
             //    toDrop = ModContent.ItemType<ZincBar>();
-            //    DustType = ModContent.DustType<Dusts.ZincDust>();
-            //    break;
-            //case 21:
+                DustType = ModContent.DustType<Dusts.ZincDust>();
+                break;
+            case 21:
             //    toDrop = ModContent.ItemType<BismuthBar>();
-            //    DustType = ModContent.DustType<Dusts.BismuthDust>();
-            //    break;
-            //case 22:
+                DustType = ModContent.DustType<Dusts.BismuthDust>();
+                break;
+            case 22:
             //    toDrop = ModContent.ItemType<IridiumBar>();
-            //    DustType = ModContent.DustType<Dusts.IridiumDust>();
-            //    break;
-            //case 23:
+                DustType = ModContent.DustType<Dusts.IridiumDust>();
+                break;
+            case 23:
             //    toDrop = ModContent.ItemType<XanthophyteBar>();
-            //    DustType = DustID.KryptonMoss;
-            //    break;
-            //case 24:
+                DustType = DustID.KryptonMoss;
+                break;
+            case 24:
             //    toDrop = ModContent.ItemType<CorruptedBar>();
-            //    DustType = DustID.Corruption;
-            //    break;
+                DustType = DustID.Corruption;
+                break;
         }
-        Item.NewItem(WorldGen.GetItemSource_FromTileBreak(i * 16, j * 16), i * 16, j * 16, 16, 16, toDrop);
+        //Item.NewItem(WorldGen.GetItemSource_FromTileBreak(i * 16, j * 16), i * 16, j * 16, 16, 16, toDrop);
     }
 
     public override bool CreateDust(int i, int j, ref int type)
