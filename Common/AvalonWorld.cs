28ac8531227059c7a7c293bdafbec9a5ec4c51db
ghasttear1 <23445208+ghasttear1@users.noreply.github.com>
2023-04-26 14:04:19 +1000

    world evil selection sets relevant values now

--- Common/AvalonWorld.cs
+++ Common/AvalonWorld.cs
@@ -1,254 +1,245 @@
-﻿using Avalon.Items.Placeable.Seed;
+﻿using System;
+using Avalon.Hooks;
+using Avalon.Items.Placeable.Seed;
 using Avalon.Items.Placeable.Tile.LargeHerbs;
 using Avalon.Tiles;
+using Avalon.WorldGeneration.Enums;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
-namespace Avalon.Common; 
+namespace Avalon.Common;
 
-public class AvalonWorld : ModSystem
-{
-    public enum CopperVariant
-    {
-        copper,
-        tin,
-        bronze,
-        random
+public class AvalonWorld : ModSystem {
+    public enum CopperVariant {
+        Copper,
+        Tin,
+        Bronze,
+        Random,
     }
-    public enum IronVariant
-    {
-        iron,
-        lead,
-        nickel,
-        random
+
+    public enum IronVariant {
+        Iron,
+        Lead,
+        Nickel,
+        Random,
     }
-    public enum SilverVariant
-    {
-        silver,
-        tungsten,
-        zinc,
-        random
+
+    public enum SilverVariant {
+        Silver,
+        Tungsten,
+        Zinc,
+        Random,
     }
-    public enum GoldVariant
-    {
-        gold,
-        platinum,
-        bismuth,
-        random
+
+    public enum GoldVariant {
+        Gold,
+        Platinum,
+        Bismuth,
+        Random,
     }
-    public enum RhodiumVariant
-    {
+
+    public enum RhodiumVariant {
         Rhodium = 0,
         Osmium = 1,
-        Iridium = 2
+        Iridium = 2,
     }
 
-    public static CopperVariant copperOre = CopperVariant.random;
-    public static IronVariant ironOre = IronVariant.random;
-    public static SilverVariant silverOre = SilverVariant.random;
-    public static GoldVariant goldOre = GoldVariant.random;
+    public WorldEvil WorldEvil;
+
+    public static CopperVariant CopperOre = CopperVariant.Random;
+    public static IronVariant IronOre = IronVariant.Random;
+    public static SilverVariant SilverOre = SilverVariant.Random;
+    public static GoldVariant GoldOre = GoldVariant.Random;
     public static RhodiumVariant? RhodiumOre { get; set; }
 
-    public override void SaveWorldData(TagCompound tag)
-    {
+    public override void SaveWorldData(TagCompound tag) {
         tag["RhodiumOre"] = (byte?)RhodiumOre;
-    }
-    public override void LoadWorldData(TagCompound tag)
-    {
-        if (tag.ContainsKey("RhodiumOre"))
-        {
-            RhodiumOre = (RhodiumVariant)tag.Get<byte>("RhodiumOre");
-        }
+        tag["WorldEvil"] = (byte)WorldEvil;
     }
 
-    public override void PostUpdateWorld()
-    {
+    public override void LoadWorldData(TagCompound tag) {
+        if (tag.ContainsKey("RhodiumOre")) {
+            RhodiumOre = (RhodiumVariant)tag.Get<byte>("RhodiumOre");
+        }
+
+        WorldEvil = (WorldEvil)(tag.Get<byte?>("WorldEvil") ?? WorldGen.WorldGenParam_Evil);
+    }
+
+    /// <inheritdoc />
+    public override void PreWorldGen() {
+        WorldEvil = ModContent.GetInstance<ContagionSelectionMenu>().SelectedWorldEvil switch {
+            ContagionSelectionMenu.WorldEvilSelection.Random => Main.rand.Next(Enum.GetValues<WorldEvil>()),
+            ContagionSelectionMenu.WorldEvilSelection.Corruption => WorldEvil.Corruption,
+            ContagionSelectionMenu.WorldEvilSelection.Crimson => WorldEvil.Crimson,
+            ContagionSelectionMenu.WorldEvilSelection.Contagion => WorldEvil.Contagion,
+            _ => throw new ArgumentOutOfRangeException(),
+        };
+
+        WorldGen.WorldGenParam_Evil = (int)WorldEvil;
+    }
+
+    public override void PostUpdateWorld() {
         float num2 = 3E-05f * (float)WorldGen.GetWorldUpdateRate();
 
         // float num3 = 1.5E-05f * (float)Main.worldRate;
-        for (int num4 = 0; num4 < Main.maxTilesX * Main.maxTilesY * num2; num4++)
-        {
+        for (int num4 = 0; num4 < Main.maxTilesX * Main.maxTilesY * num2; num4++) {
             int num5 = WorldGen.genRand.Next(10, Main.maxTilesX - 10);
             int num6 = WorldGen.genRand.Next(10, /*(int)Main.worldSurface - 1*/ Main.maxTilesY - 20);
             int num7 = num5 - 1;
             int num8 = num5 + 2;
             int num9 = num6 - 1;
             int num10 = num6 + 2;
-            if (num7 < 10)
-            {
+            if (num7 < 10) {
                 num7 = 10;
             }
 
-            if (num8 > Main.maxTilesX - 10)
-            {
+            if (num8 > Main.maxTilesX - 10) {
                 num8 = Main.maxTilesX - 10;
             }
 
-            if (num9 < 10)
-            {
+            if (num9 < 10) {
                 num9 = 10;
             }
 
-            if (num10 > Main.maxTilesY - 10)
-            {
+            if (num10 > Main.maxTilesY - 10) {
                 num10 = Main.maxTilesY - 10;
             }
 
             #region large herb growth
             if (Main.tile[num5, num6].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>() ||
                 Main.tile[num5, num6].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>() ||
-                Main.tile[num5, num6].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>())
-            {
+                Main.tile[num5, num6].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>()) {
                 GrowLargeHerb(num5, num6);
             }
             #endregion large herb growth
         }
     }
 
-    public static void GrowLargeHerb(int x, int y)
-    {
-        if (Main.tile[x, y].HasTile)
-        {
-            if (Main.tile[x, y].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>() && WorldGen.genRand.NextBool(8)) // phase 1 to 2
+    public static void GrowLargeHerb(int x, int y) {
+        if (Main.tile[x, y].HasTile) {
+            if (Main.tile[x, y].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>() &&
+                WorldGen.genRand.NextBool(8)) // phase 1 to 2
             {
                 bool grow = false;
                 if (Main.tile[x, y].TileFrameX == 108) // shiverthorn check
                 {
-                    if (Main.rand.NextBool(2))
-                    {
+                    if (Main.rand.NextBool(2)) {
                         grow = true;
                     }
                 }
-                else
-                {
+                else {
                     grow = true;
                 }
-                if (grow)
-                {
+
+                if (grow) {
                     Main.tile[x, y].TileType = (ushort)ModContent.TileType<LargeHerbsStage2>();
-                    if (Main.tile[x, y + 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>())
-                    {
+                    if (Main.tile[x, y + 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>()) {
                         Main.tile[x, y + 1].TileType = (ushort)ModContent.TileType<LargeHerbsStage2>();
                     }
 
-                    if (Main.tile[x, y + 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>())
-                    {
+                    if (Main.tile[x, y + 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>()) {
                         Main.tile[x, y + 2].TileType = (ushort)ModContent.TileType<LargeHerbsStage2>();
                     }
 
-                    if (Main.tile[x, y - 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>())
-                    {
+                    if (Main.tile[x, y - 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>()) {
                         Main.tile[x, y - 1].TileType = (ushort)ModContent.TileType<LargeHerbsStage2>();
                     }
 
-                    if (Main.tile[x, y - 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>())
-                    {
+                    if (Main.tile[x, y - 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage1>()) {
                         Main.tile[x, y - 2].TileType = (ushort)ModContent.TileType<LargeHerbsStage2>();
                     }
 
-                    if (Main.netMode == NetmodeID.Server)
-                    {
+                    if (Main.netMode == NetmodeID.Server) {
                         NetMessage.SendTileSquare(-1, x, y, 2);
                         NetMessage.SendTileSquare(-1, x, y + 1, 2);
                         NetMessage.SendTileSquare(-1, x, y + 2, 2);
                         NetMessage.SendTileSquare(-1, x, y - 1, 2);
                         NetMessage.SendTileSquare(-1, x, y - 2, 2);
                     }
+
                     WorldGeneration.Utils.SquareTileFrameArea(x, y, 4, true);
-                    return;
                 }
             }
-            else if (Main.tile[x, y].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>() && WorldGen.genRand.NextBool(7)) // phase 2 to 3
+            else if (Main.tile[x, y].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>() &&
+                     WorldGen.genRand.NextBool(7)) // phase 2 to 3
             {
                 bool grow = false;
                 if (Main.tile[x, y].TileFrameX == 108) // shiverthorn check
                 {
-                    if (Main.rand.NextBool(2))
-                    {
+                    if (Main.rand.NextBool(2)) {
                         grow = true;
                     }
                 }
-                else
-                {
+                else {
                     grow = true;
                 }
-                if (grow)
-                {
+
+                if (grow) {
                     Main.tile[x, y].TileType = (ushort)ModContent.TileType<LargeHerbsStage3>();
-                    if (Main.tile[x, y + 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>())
-                    {
+                    if (Main.tile[x, y + 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>()) {
                         Main.tile[x, y + 1].TileType = (ushort)ModContent.TileType<LargeHerbsStage3>();
                     }
 
-                    if (Main.tile[x, y + 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>())
-                    {
+                    if (Main.tile[x, y + 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>()) {
                         Main.tile[x, y + 2].TileType = (ushort)ModContent.TileType<LargeHerbsStage3>();
                     }
 
-                    if (Main.tile[x, y - 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>())
-                    {
+                    if (Main.tile[x, y - 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>()) {
                         Main.tile[x, y - 1].TileType = (ushort)ModContent.TileType<LargeHerbsStage3>();
                     }
 
-                    if (Main.tile[x, y - 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>())
-                    {
+                    if (Main.tile[x, y - 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage2>()) {
                         Main.tile[x, y - 2].TileType = (ushort)ModContent.TileType<LargeHerbsStage3>();
                     }
 
-                    if (Main.netMode == NetmodeID.Server)
-                    {
+                    if (Main.netMode == NetmodeID.Server) {
                         NetMessage.SendTileSquare(-1, x, y, 2);
                         NetMessage.SendTileSquare(-1, x, y + 1, 2);
                         NetMessage.SendTileSquare(-1, x, y + 2, 2);
                         NetMessage.SendTileSquare(-1, x, y - 1, 2);
                         NetMessage.SendTileSquare(-1, x, y - 2, 2);
                     }
+
                     WorldGeneration.Utils.SquareTileFrameArea(x, y, 4, true);
-                    return;
                 }
             }
-            else if (Main.tile[x, y].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>() && WorldGen.genRand.NextBool(5)) // phase 3 to 4
+            else if (Main.tile[x, y].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>() &&
+                     WorldGen.genRand.NextBool(5)) // phase 3 to 4
             {
                 Main.tile[x, y].TileType = (ushort)ModContent.TileType<LargeHerbsStage4>();
-                if (Main.tile[x, y + 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>())
-                {
+                if (Main.tile[x, y + 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>()) {
                     Main.tile[x, y + 1].TileType = (ushort)ModContent.TileType<LargeHerbsStage4>();
                 }
 
-                if (Main.tile[x, y + 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>())
-                {
+                if (Main.tile[x, y + 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>()) {
                     Main.tile[x, y + 2].TileType = (ushort)ModContent.TileType<LargeHerbsStage4>();
                 }
 
-                if (Main.tile[x, y - 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>())
-                {
+                if (Main.tile[x, y - 1].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>()) {
                     Main.tile[x, y - 1].TileType = (ushort)ModContent.TileType<LargeHerbsStage4>();
                 }
 
-                if (Main.tile[x, y - 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>())
-                {
+                if (Main.tile[x, y - 2].TileType == (ushort)ModContent.TileType<LargeHerbsStage3>()) {
                     Main.tile[x, y - 2].TileType = (ushort)ModContent.TileType<LargeHerbsStage4>();
                 }
 
-                if (Main.netMode == NetmodeID.Server)
-                {
+                if (Main.netMode == NetmodeID.Server) {
                     NetMessage.SendTileSquare(-1, x, y, 2);
                     NetMessage.SendTileSquare(-1, x, y + 1, 2);
                     NetMessage.SendTileSquare(-1, x, y + 2, 2);
                     NetMessage.SendTileSquare(-1, x, y - 1, 2);
                     NetMessage.SendTileSquare(-1, x, y - 2, 2);
                 }
+
                 WorldGeneration.Utils.SquareTileFrameArea(x, y, 4, true);
-                return;
             }
         }
     }
-    public static void CheckLargeHerb(int x, int y, int type)
-    {
-        if (WorldGen.destroyObject)
-        {
+
+    public static void CheckLargeHerb(int x, int y, int type) {
+        if (WorldGen.destroyObject) {
             return;
         }
 
@@ -259,27 +250,22 @@ public class AvalonWorld : ModSystem
         int yFrame = Main.tile[x, y].TileFrameY;
         fixedY -= yFrame / 18;
         if (!WorldGen.SolidTile2(x, fixedY + 3) || !Main.tile[x, fixedY].HasTile ||
-            !Main.tile[x, fixedY + 1].HasTile || !Main.tile[x, fixedY + 2].HasTile)
-        {
+            !Main.tile[x, fixedY + 1].HasTile || !Main.tile[x, fixedY + 2].HasTile) {
             destroy = true;
         }
 
-        if (destroy)
-        {
+        if (destroy) {
             WorldGen.destroyObject = true;
-            for (int u = fixedY; u < fixedY + 3; u++)
-            {
+            for (int u = fixedY; u < fixedY + 3; u++) {
                 WorldGen.KillTile(x, u);
             }
 
             // 469 through 471 are the immature tiles of the large herb; 472 is the mature version
             if (type == (ushort)ModContent.TileType<LargeHerbsStage1>() ||
                 type == (ushort)ModContent.TileType<LargeHerbsStage2>() ||
-                type == (ushort)ModContent.TileType<LargeHerbsStage3>())
-            {
+                type == (ushort)ModContent.TileType<LargeHerbsStage3>()) {
                 int item = 0;
-                switch (style)
-                {
+                switch (style) {
                     case 0:
                         item = ModContent.ItemType<LargeDaybloomSeed>();
                         break;
@@ -315,16 +301,13 @@ public class AvalonWorld : ModSystem
                         break;
                 } // 3710 through 3719 are the seeds
 
-                if (item > 0)
-                {
+                if (item > 0) {
                     Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, fixedY * 16, 16, 48, item);
                 }
             }
-            else
-            {
+            else {
                 int item = 0;
-                switch (style)
-                {
+                switch (style) {
                     case 0:
                         item = ModContent.ItemType<LargeDaybloom>();
                         break;
@@ -360,8 +343,7 @@ public class AvalonWorld : ModSystem
                         break;
                 }
 
-                if (item > 0)
-                {
+                if (item > 0) {
                     Item.NewItem(WorldGen.GetItemSource_FromTileBreak(x, y), x * 16, fixedY * 16, 16, 48, item);
                 }
 
