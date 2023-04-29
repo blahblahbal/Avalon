0d3773ede5e1ec12585012b855317fb6e02b07f1
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-04-29 13:05:18 -0500

    Bacterium Prim part 3 (MULTiplayer testing editoon :smiley: )

--- Projectiles/Hostile/CorrosiveMucus.cs
+++ Projectiles/Hostile/CorrosiveMucus.cs
@@ -20,10 +20,23 @@ public class CorrosiveMucus : ModProjectile
     {
         if(Projectile.tileCollide)
         Projectile.velocity.Y += 0.05f;
-        int D = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CorruptGibs, Projectile.velocity.X, Projectile.velocity.Y);
-        Main.dust[D].noGravity = true;
-        Main.dust[D].scale = 1.5f;
-        Main.dust[D].alpha = 128;
+
+        if (Projectile.ai[1] == 0)
+        {
+            int D2 = Dust.NewDust(Projectile.Center + new Vector2(0,-4), 0, 0, DustID.CorruptGibs, Projectile.velocity.X, Projectile.velocity.Y);
+            Main.dust[D2].noGravity = true;
+            Main.dust[D2].velocity = Projectile.velocity * 0.5f;
+            Main.dust[D2].scale = 1.5f;
+            Main.dust[D2].alpha = 128;
+        }
+        else
+        {
+            int D = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CorruptGibs, Projectile.velocity.X, Projectile.velocity.Y);
+            Main.dust[D].noGravity = true;
+            Main.dust[D].scale = 1.5f;
+            Main.dust[D].alpha = 128;
+        }
+        Lighting.AddLight(Projectile.Center, 0.3f, 0.35f, 0f);
     }
 
     public override void OnHitPlayer(Player target, Player.HurtInfo info)
@@ -39,9 +52,10 @@ public class CorrosiveMucus : ModProjectile
 
     public override bool OnTileCollide(Vector2 oldVelocity)
     {
+        Projectile.ai[1]++;
         SoundEngine.PlaySound(SoundID.NPCHit1, Projectile.Center);
         Projectile.timeLeft = 16 * 60;
-        Projectile.Size *= 2f;
+        Projectile.Resize(32, 24);
         Projectile.velocity *= 0;
         Projectile.tileCollide = false;
         return false;
