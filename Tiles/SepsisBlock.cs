1fe339a62ae23bef8de7d068ba15e41fd037d6cf
TerrorPenguin <terra_penguin@live.com>
2023-05-03 17:09:23 +1000

    Sepsis Block

---
+++ Tiles/SepsisBlock.cs
@@ -0,0 +1,22 @@
+using Avalon.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Tiles;
+
+public class SepsisBlock : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        AddMapEntry(new Color(126, 132, 72));
+        Main.tileSolid[Type] = true;
+        Main.tileMergeDirt[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        Main.tileBrick[Type] = true;
+        ItemDrop = ModContent.ItemType<Items.Placeable.Tile.SepsisBlock>();
+        DustType = ModContent.DustType<ContagionDust>();
+        HitSound = SoundID.NPCDeath1;
+    }
+}
