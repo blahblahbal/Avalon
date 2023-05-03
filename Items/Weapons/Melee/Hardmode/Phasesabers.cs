f34851e0d343fe936b5cf4086656c6169b5352c6
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-05-02 22:43:54 -0500

    Phaseblades & Sabers

---
+++ Items/Weapons/Melee/Hardmode/Phasesabers.cs
@@ -0,0 +1,69 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Avalon.Items.Weapons.Melee.PreHardmode;
+
+namespace Avalon.Items.Weapons.Melee.Hardmode
+{
+    internal class Phasesabers
+    {
+        public class BrownPhasesaber : ModItem
+        {
+            public override void SetDefaults()
+            {
+                Item.CloneDefaults(ItemID.RedPhasesaber);
+            }
+            public override Color? GetAlpha(Color lightColor)
+            {
+                return Color.White;
+            }
+            public override void AddRecipes()
+            {
+                Recipe.Create(Type).AddIngredient(ModContent.ItemType<BrownPhaseblade>()).AddIngredient(ItemID.CrystalShard, 50).AddTile(TileID.Anvils).Register();
+            }
+            public override void MeleeEffects(Player player, Rectangle hitbox)
+            {
+                Lighting.AddLight((int)((player.itemLocation.X + 6f + player.velocity.X) / 16f), (int)((player.itemLocation.Y - 14f) / 16f), 0.5f, 0.3f, 0f);
+            }
+        }
+        public class CyanPhasesaber : ModItem
+        {
+            public override void SetDefaults()
+            {
+                Item.CloneDefaults(ItemID.RedPhasesaber);
+            }
+            public override Color? GetAlpha(Color lightColor)
+            {
+                return Color.White;
+            }
+            public override void AddRecipes()
+            {
+                Recipe.Create(Type).AddIngredient(ModContent.ItemType<CyanPhaseblade>()).AddIngredient(ItemID.CrystalShard, 50).AddTile(TileID.Anvils).Register();
+            }
+            public override void MeleeEffects(Player player, Rectangle hitbox)
+            {
+                Lighting.AddLight((int)((player.itemLocation.X + 6f + player.velocity.X) / 16f), (int)((player.itemLocation.Y - 14f) / 16f), 0f, 0.5f, 0.5f);
+            }
+        }
+        public class LimePhasesaber : ModItem
+        {
+            public override void SetDefaults()
+            {
+                Item.CloneDefaults(ItemID.RedPhasesaber);
+            }
+            public override Color? GetAlpha(Color lightColor)
+            {
+                return Color.White;
+            }
+            public override void AddRecipes()
+            {
+                Recipe.Create(Type).AddIngredient(ModContent.ItemType<LimePhaseblade>()).AddIngredient(ItemID.CrystalShard, 50).AddTile(TileID.Anvils).Register();
+            }
+            public override void MeleeEffects(Player player, Rectangle hitbox)
+            {
+                Lighting.AddLight((int)((player.itemLocation.X + 6f + player.velocity.X) / 16f), (int)((player.itemLocation.Y - 14f) / 16f), 0.1f, 0.5f, 0f);
+            }
+        }
+    }
+}
---
+++ Items/Weapons/Melee/PreHardmode/Phaseblades.cs
@@ -0,0 +1,66 @@
+using Avalon.Items.Material.Ores;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Items.Weapons.Melee.PreHardmode
+{
+    public class BrownPhaseblade : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.RedPhaseblade);
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+        public override void AddRecipes()
+        {
+            Recipe.Create(Type).AddIngredient(ModContent.ItemType<Zircon>(), 10).AddIngredient(ItemID.MeteoriteBar, 15).AddTile(TileID.Anvils).Register();
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            Lighting.AddLight((int)((player.itemLocation.X + 6f + player.velocity.X) / 16f), (int)((player.itemLocation.Y - 14f) / 16f), 0.5f, 0.3f, 0f);
+        }
+    }
+    public class CyanPhaseblade : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.RedPhaseblade);
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+        public override void AddRecipes()
+        {
+            Recipe.Create(Type).AddIngredient(ModContent.ItemType<Tourmaline>(), 10).AddIngredient(ItemID.MeteoriteBar, 15).AddTile(TileID.Anvils).Register();
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            Lighting.AddLight((int)((player.itemLocation.X + 6f + player.velocity.X) / 16f), (int)((player.itemLocation.Y - 14f) / 16f), 0f, 0.5f, 0.5f);
+        }
+    }
+    public class LimePhaseblade : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.RedPhaseblade);
+        }
+        public override Color? GetAlpha(Color lightColor)
+        {
+            return Color.White;
+        }
+        public override void AddRecipes()
+        {
+            Recipe.Create(Type).AddIngredient(ModContent.ItemType<Peridot>(), 10).AddIngredient(ItemID.MeteoriteBar, 15).AddTile(TileID.Anvils).Register();
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            Lighting.AddLight((int)((player.itemLocation.X + 6f + player.velocity.X) / 16f), (int)((player.itemLocation.Y - 14f) / 16f), 0.1f, 0.5f, 0f);
+        }
+    }
+}
