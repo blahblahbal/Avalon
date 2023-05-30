5f58f0a4ab9886ae89013f726c5ee981b3a8fa5b
TerrorPenguin <terra_penguin@live.com>
2023-05-30 16:01:44 +1000

    boltstone & starstone bricks

---
+++ Tiles/StarstoneBrick.cs
@@ -0,0 +1,19 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Tiles;
+
+public class StarstoneBrick : ModTile
+{
+    public override void SetStaticDefaults()
+    {
+        AddMapEntry(new Color(42, 102, 221));
+        Main.tileSolid[Type] = true;
+        Main.tileBlockLight[Type] = true;
+        //ItemDrop = ModContent.ItemType<Items.Placeable.Tile.StarstoneBrick>();
+        HitSound = SoundID.Tink;
+        DustType = DustID.BlueTorch;
+    }
+}
