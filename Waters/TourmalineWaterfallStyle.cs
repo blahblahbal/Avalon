254d38f9072a5fdaa5477e8e1709164fa70d7570
TerrorPenguin <terra_penguin@live.com>
2023-06-04 12:12:30 +1000

    adjusted zircon waterfall colour
    also removed insanity comment

--- Waters/TourmalineWaterfallStyle.cs
+++ Waters/TourmalineWaterfallStyle.cs
@@ -8,7 +8,7 @@ public class TourmalineWaterfallStyle : ModWaterfallStyle
 {
     public override void AddLight(int i, int j) =>
              Lighting.AddLight(new Vector2(i, j).ToWorldCoordinates(), new Vector3(0, 1f * 0.8f, 1f * 0.8f) * 0.25f);
-    public override void ColorMultiplier(ref float r, ref float g, ref float b, float a) //Does this do anything? idk tbh, don't think so lmao
+    public override void ColorMultiplier(ref float r, ref float g, ref float b, float a)
     {
         r = 0;
         g = 255f * 0.8f;
--- Waters/ZirconWaterfallStyle.cs
+++ Waters/ZirconWaterfallStyle.cs
@@ -8,10 +8,10 @@ public class ZirconWaterfallStyle : ModWaterfallStyle
 {
     public override void AddLight(int i, int j) =>
              Lighting.AddLight(new Vector2(i, j).ToWorldCoordinates(), new Vector3(1.1f * 0.8f, 0.8f * 0.8f, 0.4f * 0.8f) * 0.25f);
-    public override void ColorMultiplier(ref float r, ref float g, ref float b, float a) //Does this do anything? idk tbh, don't think so lmao
+    public override void ColorMultiplier(ref float r, ref float g, ref float b, float a)
     {
-        r = 280f * 0.8f;
-        g = 204f * 0.8f;
-        b = 102f * 0.8f;
+        r = 230f * 0.8f;
+        g = 174f * 0.8f;
+        b = 92f * 0.8f;
     }
 }
