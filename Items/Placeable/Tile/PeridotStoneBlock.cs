42157c9a50c80c094775a22f91770f1ffad9cf35
TerrorPenguin <terra_penguin@live.com>
2023-06-09 12:12:02 +1000

    Gemstone blocks & walls

---
+++ Items/Placeable/Tile/PeridotStoneBlock.cs
@@ -0,0 +1,38 @@
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace Avalon.Items.Placeable.Tile;
+
+class PeridotStoneBlock : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 100;
+    }
+
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Item.autoReuse = true;
+        Item.consumable = true;
+        Item.createTile = ModContent.TileType<Tiles.Ores.Peridot>();
+        Item.width = dims.Width;
+        Item.useTurn = true;
+        Item.useTime = 10;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.maxStack = 999;
+        Item.useAnimation = 15;
+        Item.height = dims.Height;
+    }
+    public override void AddRecipes()
+    {
+        Recipe.Create(Type)
+            .AddIngredient(ModContent.ItemType<Material.Ores.Peridot>())
+            .AddIngredient(ItemID.StoneBlock)
+            .AddTile(TileID.HeavyWorkBench)
+            .AddCondition(Condition.InGraveyard)
+            .Register();
+    }
+}
---
+++ Walls/PeridotStoneWall.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Walls;
+
+public class PeridotStoneWall : ModWall
+{
+    public override void SetStaticDefaults()
+    {
+        Main.wallHouse[Type] = true;
+        Main.wallBlend[Type] = WallID.Stone;
+        AddMapEntry(new Color(52, 52, 52));
+        DustType = DustID.Stone;
+    }
+}
