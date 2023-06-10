7ac8b394b6069246f0b0049e4c74c168cd5f80fc
TerrorPenguin <terra_penguin@live.com>
2023-06-10 12:24:31 +1000

    Update Ectograss.cs
    glowmask fixed

--- Tiles/Ectograss.cs
+++ Tiles/Ectograss.cs
@@ -24,8 +24,15 @@ public class Ectograss : ModTile
         RegisterItemDrop(ItemID.AshBlock);
         DustType = DustID.Silt;
     }
-    // still needs work lol
-    public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+    public override void DrawEffects(int i, int j, SpriteBatch spriteBatch, ref TileDrawInfo drawData)
+    {
+        // This allows us to draw things in SpecialDraw
+        if (drawData.tileFrameX >= 0 && drawData.tileFrameY >= 0)
+        {
+            Main.instance.TilesRenderer.AddSpecialLegacyPoint(i, j);
+        }
+    }
+    public override void SpecialDraw(int i, int j, SpriteBatch spriteBatch)
     {
         Tile tile = Main.tile[i, j];
         var zero = new Vector2(Main.offScreenRange, Main.offScreenRange);
@@ -36,7 +43,7 @@ public class Ectograss : ModTile
 
         Vector2 pos = new Vector2(i * 16, j * 16) + zero - Main.screenPosition;
         var frame = new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, 16);
-        var halfFrame = new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, -8);
+        var halfFrame = new Rectangle(tile.TileFrameX, tile.TileFrameY, 16, 8);
         Texture2D tex = Mod.Assets.Request<Texture2D>("Tiles/Ectograss_Glow").Value;
         if (tile.Slope == SlopeType.Solid && !tile.IsHalfBlock)
         {
@@ -44,6 +51,7 @@ public class Ectograss : ModTile
         }
         else if (tile.IsHalfBlock)
         {
+            pos = new Vector2(i * 16, (j * 16) + 8) + zero - Main.screenPosition;
             Main.spriteBatch.Draw(Mod.Assets.Request<Texture2D>("Tiles/Ectograss_Glow").Value, pos, halfFrame, Color.White);
         }
         else
@@ -53,9 +61,11 @@ public class Ectograss : ModTile
             {
                 screenOffset = Vector2.Zero;
             }
-            Vector2 vector = new Vector2(i * 16 - (int)Main.screenPosition.X - ((float) - 16f) / 2f, j * 16 - (int)Main.screenPosition.Y + 0 + 8) + screenOffset;
+            Vector2 vector = new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + screenOffset;
             int slopeType = (int)tile.Slope;
             int num5 = 2;
+            int addFrY = Main.tileFrame[Type] * 90;
+            int addFrX = 0;
             for (int q = 0; q < 8; q++)
             {
                 int num6 = q * -2;
@@ -83,10 +93,10 @@ public class Ectograss : ModTile
                         num9 = 16 - q * 2 - 2;
                         break;
                 }
-                Main.spriteBatch.Draw(tex, vector + new Vector2(num9, q * num5 + num6), (Rectangle?)new Rectangle(tile.TileFrameX + 0 + num9, tile.TileFrameY + 8 + num8, num5, num7), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+                Main.spriteBatch.Draw(tex, vector + new Vector2(num9, q * num5 + num6), (Rectangle?)new Rectangle(tile.TileFrameX + addFrX + num9, tile.TileFrameY + addFrY + num8, num5, num7), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
             }
             int num10 = ((slopeType <= 2) ? 14 : 0);
-            Main.spriteBatch.Draw(tex, vector + new Vector2(0f, num10), (Rectangle?)new Rectangle(tile.TileFrameX + 8, tile.TileFrameY + 8 + num10, 16, 2), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            Main.spriteBatch.Draw(tex, vector + new Vector2(0f, num10), (Rectangle?)new Rectangle(tile.TileFrameX + addFrX, tile.TileFrameY + addFrY + num10, 16, 2), Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
         }
     }
     public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
