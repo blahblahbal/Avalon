fa0f4fee7ef44908799aa168c8f1c4d75a2593ca
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-07-29 16:16:46 -0500

    The most basic jeo weapon ever created

---
+++ Items/Weapons/Melee/PreHardmode/TetanusChakram.cs
@@ -0,0 +1,35 @@
+using Avalon.Projectiles.Melee;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Items.Weapons.Melee.PreHardmode;
+
+class TetanusChakram : ModItem
+{
+    int ShootTimes;
+    public override void SetDefaults()
+    {
+        Item.width = 38;
+        Item.height = 40;
+        Item.damage = 31;
+        Item.noUseGraphic = true;
+        Item.shootSpeed = 7f;
+        Item.rare = ItemRarityID.Blue;
+        Item.noMelee = true;
+        Item.useTime = 15;
+        Item.useAnimation = 15;
+        Item.knockBack = 2.5f;
+        Item.shoot = ModContent.ProjectileType<Projectiles.Melee.TetanusChakram>();
+        Item.DamageType = DamageClass.Melee;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.value = Item.sellPrice(0, 1, 50, 0);
+        Item.UseSound = SoundID.Item1;
+    }
+    public override bool CanUseItem(Player player)
+    {
+        return player.ownedProjectileCounts[Item.shoot] < 2;
+    }
+}
