c8d21eada052e59869ddcacbbc91029b39196e05
blahblahbal <jabawakijadehawk@gmail.com>
2023-09-18 14:00:25 -0700

    fix

--- Projectiles/Ranged/StasisShot.cs
+++ Projectiles/Ranged/StasisShot.cs
@@ -33,8 +33,8 @@ public class StasisShot : ModProjectile
             dust.scale = 1;
         }
     }
-    public override void Kill(int timeLeft)
+    public override void OnKill(int timeLeft)
     {
-        base.Kill(timeLeft);
+        base.OnKill(timeLeft);
     }
 }
