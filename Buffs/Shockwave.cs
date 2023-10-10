2da66635960c66173e2660d5ee39b6d18e9ac74a
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-10-09 21:08:09 -0500

    Shcokwave potion

--- Buffs/Shockwave.cs
+++ Buffs/Shockwave.cs
@@ -4,6 +4,10 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
+using Terraria.Audio;
+using Avalon.Common;
+using Terraria.Graphics.CameraModifiers;
+using Avalon.Particles;
 
 namespace Avalon.Buffs;
 
@@ -12,6 +16,7 @@ public class Shockwave : ModBuff
     private int fallStart_old = -1;
     public override void Update(Player player, ref int buffIndex)
     {
+
         if (Main.rand.NextBool(50))
         {
             int D = Dust.NewDust(player.position, player.width, player.height, 9, (player.velocity.X * 0.2f) + (player.direction * 3), player.velocity.Y * 1.2f, 60, new Color(), 1f);
@@ -41,23 +46,19 @@ public class Shockwave : ModBuff
             Main.dust[D4].velocity.X *= -1.2f;
         }
 
-        int sw = Main.screenWidth;
-        int sh = Main.screenHeight;
-        int sx = (int)Main.screenPosition.X;
-        int sy = (int)Main.screenPosition.Y;
-
-
         if (fallStart_old == -1) fallStart_old = player.fallStart;
         int fall_dist = 0;
         if (player.velocity.Y == 0f) // detect landing from a fall
         {
             fall_dist = (int)(((int)(player.position.Y / 16f) - fallStart_old) * player.gravDir);
+            if (fall_dist > 30) fall_dist = 30;
         }
 
         Vector2 p_pos = player.position + (new Vector2(player.width, player.height) / 2f);
         int numOfNPCs = 0;
         if (fall_dist > 3) // just fell
         {
+            float Radius = fall_dist * 35;
             for (int o = 0; o < Main.npc.Length; o++)
             {
                 // iterate through NPCs
@@ -68,21 +69,20 @@ public class Shockwave : ModBuff
                     continue;
                 }
 
-                if (N.position.X >= sx && N.position.X <= sx + sw && N.position.Y >= sy && N.position.Y <= sy + sh)
+                if (N.Center.Distance(player.Center) < Radius)
                 {
                     list.Add(N);
                 }
 
-                var n_pos = new Vector2(N.position.X + (N.width * 0.5f), N.position.Y + (N.height * 0.5f)); // NPC location
+                var n_pos = N.Center; // NPC location
                 int HitDir = -1;
                 if (n_pos.X > p_pos.X)
                 {
                     HitDir = 1;
                 }
 
-                if (N.position.X >= sx && N.position.X <= sx + sw && N.position.Y >= sy && N.position.Y <= sy + sh)
+                if (N.Center.Distance(player.Center) < Radius)
                 {
-                    // on screen
                     numOfNPCs++;
                     int multiplier = 5;
 
@@ -108,10 +108,10 @@ public class Shockwave : ModBuff
                     //    multiplier = 2;
                     //}
 
-                    N.StrikeNPC(new NPC.HitInfo {Damage = multiplier * fall_dist, Knockback = 5f, HitDirection = HitDir});
+                    int dmg = N.StrikeNPC(new NPC.HitInfo {Damage = (int)(multiplier * fall_dist * -((N.Center.Distance(player.Center) / Radius) - 1)) * 2, Knockback = fall_dist * -((N.Center.Distance(player.Center) / Radius) - 1) * 2.7f, HitDirection = HitDir});
                     if (Main.netMode != 0)
                     {
-                        NetMessage.SendData(MessageID.DamageNPC, -1, -1, null, o, multiplier * fall_dist, 10f, HitDir); // for multiplayer support
+                        NetMessage.SendData(MessageID.DamageNPC, -1, -1, null, o, dmg, 10f, HitDir); // for multiplayer support
                     }
 
                     
@@ -119,6 +119,18 @@ public class Shockwave : ModBuff
                     // optionally add debuff here
                 } // END on screen
             } // END iterate through NPCs
+
+            var Sound = SoundEngine.PlaySound(SoundID.Item14, player.position);
+            ParticleSystem.AddParticle(new ShockwaveParticle(), player.Center + new Vector2(0,(player.height * 0.5f * player.gravDir)),Vector2.Zero,default,Radius);
+            if (SoundEngine.TryGetActiveSound(Sound, out ActiveSound sound) && sound != null && sound.IsPlaying)
+            {
+                sound.Volume = MathHelper.Clamp(fall_dist / 7f,0.2f,3);
+            }
+            if (player.whoAmI == Main.myPlayer && ModContent.GetInstance<AvalonClientConfig>().AdditionalScreenshakes)
+            {
+                PunchCameraModifier modifier = new PunchCameraModifier(player.Center, new Vector2(Main.rand.NextFloat(-0.3f,0.3f), fall_dist / 2f), 1f, 3f, 15, 300f, player.name);
+                Main.instance.CameraModifiers.Add(modifier);
+            }
         } // END just fell
         fallStart_old = player.fallStart;
     }
