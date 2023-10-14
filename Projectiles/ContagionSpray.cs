237ce7345a6d6aae33e83b7610c9f7eb70aa1513
ghasttear1 <23445208+ghasttear1@users.noreply.github.com>
2023-10-14 11:27:39 +1100

    Clean up jungle and contagion solution logic.

--- Projectiles/ContagionSpray.cs
+++ Projectiles/ContagionSpray.cs
@@ -1,4 +1,6 @@
-using Avalon.Common;
+using System;
+using Avalon.Dusts;
+using Avalon.WorldGeneration.Helpers;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
@@ -7,74 +9,54 @@ namespace Avalon.Projectiles;
 
 public class ContagionSpray : ModProjectile
 {
+    private int Progress
+    {
+        get => (int)Projectile.ai[0];
+        set => Projectile.ai[0] = value;
+    }
+
     public override void SetDefaults()
     {
-        Rectangle dims = this.GetDims();
-        Projectile.width = dims.Width * 6 / 16;
-        Projectile.height = dims.Height * 6 / 16 / Main.projFrames[Projectile.type];
-        Projectile.aiStyle = -1;
-        Projectile.friendly = true;
-        Projectile.alpha = 255;
-        Projectile.penetrate = -1;
-        Projectile.MaxUpdates = 2;
-        Projectile.tileCollide = false;
-        Projectile.ignoreWater = true;
+        Projectile.DefaultToSpray();
+        Projectile.aiStyle = 0;
     }
-    public override bool? CanDamage()
-    {
-        return false;
-    }
-    public override bool? CanCutTiles()
-    {
-        return false;
-    }
+
     public override void AI()
     {
-        int num500 = ModContent.DustType<Dusts.YellowSolutionDust>();
+        var dustType = ModContent.DustType<YellowSolutionDust>();
+
         if (Projectile.owner == Main.myPlayer)
         {
-            AvalonWorld.Convert((int)(Projectile.position.X + Projectile.width / 2) / 16, (int)(Projectile.position.Y + Projectile.height / 2) / 16, 0, 2);
+            ConversionHelper.ConvertToContagion((int)(Projectile.position.X + Projectile.width * 0.5f) / 16, (int)(Projectile.position.Y + Projectile.height * 0.5f) / 16, 2);
         }
-        if (Projectile.timeLeft > 133)
+
+        Projectile.timeLeft = Math.Min(Projectile.timeLeft, 133);
+
+        if (Progress > 7)
         {
-            Projectile.timeLeft = 133;
-        }
-        if (Projectile.ai[0] > 7f)
-        {
-            var num502 = 1f;
-            if (Projectile.ai[0] == 8f)
-            {
-                num502 = 0.2f;
-            }
-            else if (Projectile.ai[0] == 9f)
-            {
-                num502 = 0.4f;
-            }
-            else if (Projectile.ai[0] == 10f)
-            {
-                num502 = 0.6f;
-            }
-            else if (Projectile.ai[0] == 11f)
-            {
-                num502 = 0.8f;
-            }
-            Projectile.ai[0] += 1f;
-            for (var num503 = 0; num503 < 1; num503++)
-            {
-                var num504 = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, num500, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100, default(Color), 1f);
-                Main.dust[num504].noGravity = true;
-                Main.dust[num504].scale *= 1.75f;
-                var dust57 = Main.dust[num504];
-                dust57.velocity.X = dust57.velocity.X * 2f;
-                var dust58 = Main.dust[num504];
-                dust58.velocity.Y = dust58.velocity.Y * 2f;
-                Main.dust[num504].scale *= num502;
-            }
+            var dustScale = Progress switch{
+                8 => 0.2f,
+                9 => 0.4f,
+                10 => 0.6f,
+                11 => 0.8f,
+                _ => 1f
+            };
+
+            Progress++;
+
+            var dust = Dust.NewDustDirect(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, dustType, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100);
+
+            dust.noGravity = true;
+            dust.scale *= 1.75f;
+            dust.velocity.X *= 2f;
+            dust.velocity.Y *= 2f;
+            dust.scale *= dustScale;
         }
         else
         {
-            Projectile.ai[0] += 1f;
+            Progress++;
         }
+
         Projectile.rotation += 0.3f * Projectile.direction;
     }
 }
---
+++ WorldGeneration/Helpers/ConversionHelper.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Avalon.Tiles;
+using Avalon.Tiles.Contagion;
+using Avalon.Walls;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Tile = Avalon.Data.Sets.Tile;
+
+namespace Avalon.WorldGeneration.Helpers;
+
+public static class ConversionHelper
+{
+    public static void ConvertToJungle(int i, int j, int size = 4)
+    {
+        for (var k = i - size; k <= i + size; k++)
+        {
+            for (var l = j - size; l <= j + size; l++)
+            {
+                if (!WorldGen.InWorld(k, l, 1) || Math.Abs(k - i) + Math.Abs(l - j) >= Math.Sqrt(size * size + size * size))
+                    continue;
+
+                int type = Main.tile[k, l].TileType;
+                int wall = Main.tile[k, l].WallType;
+
+                if (WallID.Sets.Conversion.Grass[wall])
+                {
+                    Main.tile[k, l].WallType = WallID.JungleUnsafe;
+                    WorldGen.SquareWallFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (WallID.Sets.Conversion.Dirt[wall])
+                {
+                    Main.tile[k, l].WallType = WallID.MudUnsafe;
+                    WorldGen.SquareWallFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+
+                if (TileID.Sets.Conversion.Stone[type])
+                {
+                    Main.tile[k, l].TileType = TileID.Stone;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Grass[type])
+                {
+                    Main.tile[k, l].TileType = TileID.JungleGrass;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Ice[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<GreenIce>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Sand[type])
+                {
+                    Main.tile[k, l].TileType = TileID.Sand;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Sandstone[type])
+                {
+                    Main.tile[k, l].TileType = TileID.Sandstone;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.HardenedSand[type])
+                {
+                    Main.tile[k, l].TileType = TileID.HardenedSand;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (Tile.Conversion.ShortGrass[type])
+                {
+                    Main.tile[k, l].TileType = TileID.JunglePlants;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+
+                if (type is TileID.Plants2 or TileID.HallowedPlants2)
+                {
+                    Main.tile[k, l].TileType = TileID.JunglePlants2;
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+            }
+        }
+    }
+
+    public static void ConvertToContagion(int i, int j, int size = 4)
+    {
+        for (var k = i - size; k <= i + size; k++)
+        {
+            for (var l = j - size; l <= j + size; l++)
+            {
+                if (!WorldGen.InWorld(k, l, 1) || Math.Abs(k - i) + Math.Abs(l - j) >= Math.Sqrt(size * size + size * size))
+                    continue;
+
+                int type = Main.tile[k, l].TileType;
+                int wall = Main.tile[k, l].WallType;
+
+                // Check walls
+                if (WallID.Sets.Conversion.Stone[wall])
+                {
+                    Main.tile[k, l].WallType = (ushort)ModContent.WallType<ChunkstoneWall>();
+                    WorldGen.SquareWallFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (WallID.Sets.Conversion.Grass[wall])
+                {
+                    Main.tile[k, l].WallType = (ushort)ModContent.WallType<ContagionGrassWall>();
+                    WorldGen.SquareWallFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+
+                // Check tiles
+                if (type == TileID.Stalactite && (Main.tile[k, l].TileFrameX >= 54 && Main.tile[k, l].TileFrameX <= 90 || Main.tile[k, l].TileFrameX >= 216 && Main.tile[k, l].TileFrameX <= 360))
+                {
+                    if (Main.tile[k, l].TileFrameX >= 54 && Main.tile[k, l].TileFrameX <= 90)
+                    {
+                        Main.tile[k, l].TileFrameX -= 54;
+                    }
+                    if (Main.tile[k, l].TileFrameX >= 216 && Main.tile[k, l].TileFrameX <= 252)
+                    {
+                        Main.tile[k, l].TileFrameX -= 216;
+                    }
+                    if (Main.tile[k, l].TileFrameX >= 270 && Main.tile[k, l].TileFrameX <= 306)
+                    {
+                        Main.tile[k, l].TileFrameX -= 270;
+                    }
+                    if (Main.tile[k, l].TileFrameX >= 324 && Main.tile[k, l].TileFrameX <= 360)
+                    {
+                        Main.tile[k, l].TileFrameX -= 324;
+                    }
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<ContagionStalactgmites>();
+                }
+                //else 
+                if (Tile.Conversion.Vines[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<ContagionVines>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                if (TileID.Sets.Conversion.Stone[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<Chunkstone>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Grass[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<Ickgrass>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Ice[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<YellowIce>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Sand[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<Snotsand>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.Sandstone[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<Snotsandstone>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (TileID.Sets.Conversion.HardenedSand[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<HardenedSnotsand>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+                else if (Tile.Conversion.ShortGrass[type])
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<ContagionShortGrass>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+
+                if (type is TileID.Plants2 or TileID.HallowedPlants2)
+                {
+                    Main.tile[k, l].TileType = (ushort)ModContent.TileType<ContagionShortGrass>();
+                    WorldGen.SquareTileFrame(k, l);
+                    NetMessage.SendTileSquare(-1, k, l, 1);
+                }
+            }
+        }
+    }
+}
