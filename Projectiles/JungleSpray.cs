fb9b9891a03b1eddebeffa0df7d6be0f1e6ed021
ghasttear1 <23445208+ghasttear1@users.noreply.github.com>
2023-10-21 17:18:10 +1100

    JungleSpray.cs now matching AI logic for Contagion Spray

--- Projectiles/JungleSpray.cs
+++ Projectiles/JungleSpray.cs
@@ -2,6 +2,7 @@ using System;
 using Avalon.WorldGeneration.Helpers;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Avalon.Projectiles;
@@ -14,36 +15,57 @@ public class JungleSpray : ModProjectile
         set => Projectile.ai[0] = value;
     }
 
+    public override void SetStaticDefaults()
+    {
+        ProjectileID.Sets.WindPhysicsImmunity[Projectile.type] = true;
+    }
+
     public override void SetDefaults()
     {
         Projectile.DefaultToSpray();
         Projectile.aiStyle = 0;
     }
 
+    public override bool? CanDamage()
+    {
+        return false;
+    }
+    
     public override void AI()
     {
+        var largerSize = (int)Projectile.ai[1] == 1;
         var dustType = ModContent.DustType<Dusts.JungleSpray>();
 
         if (Projectile.owner == Main.myPlayer)
         {
-            ConversionHelper.ConvertToJungle((int)(Projectile.position.X + Projectile.width * 0.5f) / 16, (int)(Projectile.position.Y + Projectile.height * 0.5f) / 16, 2);
+            var size = largerSize ? 3 : 2;
+            var point = Projectile.Center.ToTileCoordinates();
+            ConversionHelper.ConvertToJungle(point.X, point.Y, size);
         }
 
         Projectile.timeLeft = Math.Min(Projectile.timeLeft, 133);
+        var progressThreshold = largerSize ? 3 : 7;
 
-        if (Progress > 7)
+        if (Progress > progressThreshold)
         {
-            var dustScale = Progress switch{
-                8 => 0.2f,
-                9 => 0.4f,
-                10 => 0.6f,
-                11 => 0.8f,
+            var dustScale = (Progress - progressThreshold) switch{
+                1 => 0.2f,
+                2 => 0.4f,
+                3 => 0.6f,
+                4 => 0.8f,
                 _ => 1f
             };
 
+            var dustOffset = 0;
+            if (largerSize)
+            {
+                dustScale *= 1.2f;
+                dustOffset = (int)(12 * dustScale);
+            }
+
             Progress++;
 
-            var dust = Dust.NewDustDirect(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, dustType, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100);
+            var dust = Dust.NewDustDirect(new Vector2(Projectile.position.X - dustOffset, Projectile.position.Y - dustOffset), Projectile.width + dustOffset * 2, Projectile.height + dustOffset * 2, dustType, Projectile.velocity.X * 0.2f, Projectile.velocity.Y * 0.2f, 100);
 
             dust.noGravity = true;
             dust.scale *= 1.75f;
