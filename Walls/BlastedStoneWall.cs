a789befd3e502d7ddd66aeb398c626f2b0c1614c
TerrorPenguin <terra_penguin@live.com>
2023-10-24 20:39:34 +1000

    blasted stone and crystal stone walls

---
+++ Walls/BlastedStoneWall.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Walls;
+
+public class BlastedStoneWall : ModWall
+{
+    public override void SetStaticDefaults()
+    {
+        Main.wallHouse[Type] = true;
+        AddMapEntry(new Color(10, 10, 10));
+        DustType = DustID.Wraith;
+    }
+}
