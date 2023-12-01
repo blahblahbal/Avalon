253672ef1a89e50337bdb4e5001505f8276c522f
Jacob Jacobsphere <geometricwarrior@gmail.com>
2023-12-01 07:53:14 -0600

    Marrow Masher

---
+++ Items/Weapons/Melee/PreHardmode/MarrowMasher.cs
@@ -0,0 +1,63 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Audio;
+
+namespace Avalon.Items.Weapons.Melee.PreHardmode;
+
+class MarrowMasher : ModItem
+{
+    //public override void SetStaticDefaults()1
+    //{
+    //    DisplayName.SetDefault("Marrow Masher");
+    //    Tooltip.SetDefault("Critical strikes have increased knockback");
+    //}
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Item.UseSound = SoundID.Item1;
+        Item.DamageType = DamageClass.Melee;
+        Item.damage = 35;
+        Item.autoReuse = true;
+        Item.scale = 1.25f;
+        Item.crit = 6;
+        Item.shootSpeed = 6f; //so the knockback works properly
+        Item.rare = ItemRarityID.Green;
+        Item.noUseGraphic = true;
+        Item.noMelee = true;
+        Item.width = dims.Width;
+        Item.height = dims.Height;
+        Item.useTime = Item.useAnimation = 40;
+        Item.knockBack = 6.5f;
+        Item.shoot = ModContent.ProjectileType<Projectiles.Melee.MarrowMasher>();
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.value = Item.sellPrice(0, 0, 40, 0);
+    }
+    public override bool MeleePrefix()
+    {
+        return true;
+    }
+
+    public int swing;
+    public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+    {
+        velocity = Vector2.Zero;
+        if (swing == 1)
+        {
+            swing--;
+            position = player.Center + new Vector2(0, 60f * Item.scale);
+        }
+        else
+        {
+            swing++;
+            position = player.Center + new Vector2(0, -60f * Item.scale);
+        }
+    }
+    public override bool CanUseItem(Player player)
+    {
+        return player.ownedProjectileCounts[Item.shoot] < 1;
+    }
+}
