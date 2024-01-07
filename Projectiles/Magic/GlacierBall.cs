23492bee7522dd25804ff074c2f7b33bb85e6c91
TerrorPenguin <terra_penguin@live.com>
2024-01-08 05:22:30 +1000

    Update GlacierBall.cs
    the gores are smaller, fewer, and randomized

--- Projectiles/Magic/GlacierBall.cs
+++ Projectiles/Magic/GlacierBall.cs
@@ -1,3 +1,4 @@
+using Avalon.Gores;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Audio;
@@ -64,9 +65,8 @@ public class GlacierBall : ModProjectile
         }
         Projectile.velocity.Y *= 0.75f;
         SoundEngine.PlaySound(SoundID.Item50, Projectile.position);
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard1").Type, Main.rand.NextFloat(0.75f, 1f));
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard2").Type, Main.rand.NextFloat(0.75f, 1f));
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard3").Type, Main.rand.NextFloat(0.75f, 1f));
+        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard" + Main.rand.Next(1, 4)).Type, Main.rand.NextFloat(0.75f, 1f));
+        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard" + Main.rand.Next(1, 4)).Type, Main.rand.NextFloat(0.75f, 1f));
         for (int i = 0; i < Main.rand.Next(3) + 5; i++)
         {
             int dust = Dust.NewDust(Projectile.position, Projectile.height, Projectile.width, DustID.IceRod, 0f, 0f, default, default, 1.25f);
@@ -82,9 +82,9 @@ public class GlacierBall : ModProjectile
     public override void OnKill(int timeLeft)
     {
         SoundEngine.PlaySound(SoundID.Item27, Projectile.position);
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.oldVelocity * 0.2f, Mod.Find<ModGore>("GlacierChunk1").Type, 1f);
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.oldVelocity * 0.2f, Mod.Find<ModGore>("GlacierChunk2").Type, 1f);
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.oldVelocity * 0.2f, Mod.Find<ModGore>("GlacierChunk3").Type, 1f);
+        int randChunk = Main.rand.Next(1, 4);
+        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.oldVelocity * 0.5f, Mod.Find<ModGore>("GlacierChunk" + randChunk).Type, 1f);
+        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.oldVelocity * 0.5f, Mod.Find<ModGore>("GlacierChunk" + (randChunk == 3 ? randChunk - Main.rand.Next(1, 3) : randChunk + 1)).Type, 1f);
     }
     public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
     {
@@ -97,9 +97,8 @@ public class GlacierBall : ModProjectile
             Projectile.velocity.Y = -Projectile.oldVelocity.Y;
         }
         Projectile.velocity.Y *= 0.75f;
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard1").Type, Main.rand.NextFloat(1f, 1.5f));
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard2").Type, Main.rand.NextFloat(1f, 1.5f));
-        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard3").Type, Main.rand.NextFloat(1f, 1.5f));
+        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard").Type, Main.rand.NextFloat(1f, 1.5f));
+        Gore.NewGore(Projectile.GetSource_FromThis(), Projectile.Center, Main.rand.NextVector2Circular(3, 3) + Projectile.velocity * 0.5f, Mod.Find<ModGore>("GlacierShard").Type, Main.rand.NextFloat(1f, 1.5f));
         Projectile.frame++;
     }
     public override Color? GetAlpha(Color lightColor)
