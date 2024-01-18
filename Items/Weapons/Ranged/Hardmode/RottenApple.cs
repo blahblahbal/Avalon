37b3d9e0490c60b5826c6f4b40f280f13b68abb8
blahblahbal <jabawakijadehawk@gmail.com>
2024-01-18 10:51:01 -0800

    add rotten apples

---
+++ Items/Weapons/Ranged/Hardmode/RottenApple.cs
@@ -0,0 +1,37 @@
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Items.Weapons.Ranged.Hardmode;
+
+class RottenApple : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Item.ResearchUnlockCount = 99;
+    }
+
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Item.damage = 20;
+        Item.noUseGraphic = true;
+        Item.maxStack = 9999;
+        Item.shootSpeed = 9f;
+        Item.DamageType = DamageClass.Ranged;
+        Item.consumable = true;
+        Item.noMelee = true;
+        Item.width = dims.Width;
+        Item.useTime = 15;
+        Item.knockBack = 3f;
+        Item.shoot = ModContent.ProjectileType<Projectiles.Ranged.RottenApple>();
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.value = 0;
+        Item.useAnimation = 15;
+        Item.height = dims.Height;
+    }
+    //public override void AddRecipes()
+    //{
+    //    CreateRecipe(10).AddIngredient(ModContent.ItemType<Material.Shards.FrostShard>()).AddTile(TileID.Anvils).Register();
+    //}
+}
---
+++ Projectiles/Ranged/RottenApple.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Projectiles.Ranged;
+
+public class RottenApple : ModProjectile
+{
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Projectile.width = dims.Width;
+        Projectile.height = dims.Height * 12 / 18 / Main.projFrames[Projectile.type];
+        Projectile.aiStyle = 2;
+        Projectile.friendly = true;
+        Projectile.penetrate = 1;
+        Projectile.DamageType = DamageClass.Ranged;
+        AIType = ProjectileID.RottenEgg;
+    }
+    public override bool? CanHitNPC(NPC target)
+    {
+        return target.lifeMax >= 1 && !target.dontTakeDamage;
+    }
+    public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+    {
+        if (target.type == NPCID.Nurse || target.type == NPCID.DoctorBones || target.type == NPCID.WitchDoctor)
+        {
+            modifiers.FinalDamage *= 3f;
+        }
+    }
+    public override void OnKill(int timeLeft)
+    {
+        SoundEngine.PlaySound(SoundID.NPCDeath1, Projectile.Center);
+        for (int i = 0; i < 10; i++)
+        {
+            int d = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.CorruptGibs, -Projectile.velocity.X / 3, -Projectile.velocity.Y / 3, Projectile.alpha);
+            Main.dust[d].noGravity = !Main.rand.NextBool(3);
+            if (Main.dust[d].noGravity)
+                Main.dust[d].fadeIn = 1f;
+        }
+    }
+}
