d0d23e91ab19c0cd51b52b4f95044810842e8b2b
blahblahbal <jabawakijadehawk@gmail.com>
2024-02-15 04:28:42 -0800

    damn

--- WorldGeneration/Passes/Tropics.cs
+++ WorldGeneration/Passes/Tropics.cs
@@ -136,55 +136,19 @@ internal class Tropics
         {
             for (int j = 150; j < Main.maxTilesY - 230; j++)
             {
-                if (Main.tile[i, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i, j].HasTile &&
-                    Main.tile[i + 1, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i + 1, j].HasTile &&
-                    Main.tile[i - 1, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i - 1, j].HasTile &&
-                    !Main.tile[i, j - 1].HasTile && !Main.tile[i - 1, j - 1].HasTile && !Main.tile[i + 1, j - 1].HasTile)
+                if (j > Main.rockLayer)
                 {
-                    if (WorldGen.genRand.NextBool(20)) WorldgenHelper.CreateLeafTrap(i, j + 2);
-                }
-                if (Main.tile[i, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i, j].HasTile &&
-                    !Main.tile[i, j - 1].HasTile && !Main.tile[i - 1, j - 1].HasTile && !Main.tile[i + 1, j - 1].HasTile)
-                {
-                    
+                    if (Main.tile[i, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i, j].HasTile &&
+                        Main.tile[i + 1, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i + 1, j].HasTile &&
+                        Main.tile[i - 1, j].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>() && Main.tile[i - 1, j].HasTile &&
+                        !Main.tile[i, j - 1].HasTile && !Main.tile[i - 1, j - 1].HasTile && !Main.tile[i + 1, j - 1].HasTile)
+                    {
+                        if (WorldGen.genRand.NextBool(5)) WorldgenHelper.CreateLeafTrap(i, j + 2);
+                    }
                 }
             }
         }
     }
-
-    //public static void TuhrtlOutpostTask(GenerationProgress progress, GameConfiguration config)
-    //{
-    //    int num562 = 0;
-    //    progress.Message = "Generating outpost";
-    //    long num563 = 0L;
-    //    double num564 = 0.25;
-    //    int y21;
-    //    int x23;
-    //    while (true)
-    //    {
-    //        y21 = WorldGen.genRand.Next((int)Main.rockLayer, Main.maxTilesY - 500);
-    //        x23 = (int)(((WorldGen.genRand.NextDouble() * num564 + 0.1) * -GenVars.dungeonSide + 0.5) * Main.maxTilesX);
-    //        if (Main.tile[x23, y21].HasTile && Main.tile[x23, y21].TileType == ModContent.TileType<Tiles.Tropics.TropicalGrass>())
-    //        {
-    //            break;
-    //        }
-    //        if (num563++ > 2000000)
-    //        {
-    //            if (num564 == 0.35)
-    //            {
-    //                num562++;
-    //                if (num562 > 10)
-    //                {
-    //                    return;
-    //                }
-    //            }
-    //            num564 = Math.Min(0.35, num564 + 0.05);
-    //            num563 = 0L;
-    //        }
-    //    }
-    //    AvalonWorld.MakeTempOutpost(x23, y21);
-    //    GenVars.structures.AddProtectedStructure(new(x23, y21, 25, 25));
-    //}
     public static void TropicsSanctumTask(GenerationProgress progress, GameConfiguration config)
     {
         progress.Message = Language.GetTextValue("Mods.Avalon.World.Generation.Tropics.Chests");
