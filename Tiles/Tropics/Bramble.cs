1235e857c9b27d6775356f13a7f06c37c13bc73b
TerrorPenguin <terra_penguin@live.com>
2024-02-15 23:45:07 +1000

    Update Bramble.cs

--- Tiles/Tropics/Bramble.cs
+++ Tiles/Tropics/Bramble.cs
@@ -10,22 +10,12 @@ public class Bramble : ModTile
 {
     public override void SetStaticDefaults()
     {
-        AddMapEntry(new Color(78, 70, 67));
+        AddMapEntry(new Color(65, 67, 24));
         TileID.Sets.TouchDamageImmediate[Type] = 20;
-        //Main.tileSolid[Type] = true;
-        Main.tileBrick[Type] = true;
         Main.tileBlockLight[Type] = true;
-        //Main.tileShine2[Type] = true;
         HitSound = SoundID.Grass;
-        DustType = DustID.JungleGrass;
-    }
-    public override void FloorVisuals(Player player)
-    {
-        //player.velocity *= 0.6f;
-    }
-    public override bool CanExplode(int i, int j)
-    {
-        return false;
+        DustType = ModContent.DustType<Dusts.TropicalDust>();
+        TileID.Sets.CanPlaceNextToNonSolidTile[Type] = true;
     }
     public override bool Slope(int i, int j)
     {
