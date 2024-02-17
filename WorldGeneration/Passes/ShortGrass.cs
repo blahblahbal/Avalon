1751b02a26e374831ec26c00e32a8af1f1587f3b
TerrorPenguin <terra_penguin@live.com>
2024-02-17 23:05:22 +1000

    short grass fixes

--- WorldGeneration/Passes/ShortGrass.cs
+++ WorldGeneration/Passes/ShortGrass.cs
@@ -1,5 +1,8 @@
+using Avalon.Data.Sets;
 using Avalon.Tiles.Contagion;
+using Avalon.Walls;
 using Terraria;
+using Terraria.ID;
 using Terraria.IO;
 using Terraria.ModLoader;
 using Terraria.WorldBuilding;
@@ -14,6 +17,7 @@ namespace Avalon.WorldGeneration.Passes
 
         protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
         {
+            int grass = ModContent.TileType<Tiles.Contagion.ContagionShortGrass>();
             for (int num244 = 0; num244 < Main.maxTilesX; num244++)
             {
                 progress.Set((double)num244 / Main.maxTilesX);
@@ -21,9 +25,24 @@ namespace Avalon.WorldGeneration.Passes
                 {
                     if (Main.tile[num244, num245].TileType == ModContent.TileType<Ickgrass>() && Main.tile[num244, num245].HasUnactuatedTile)
                     {
-                        if (!Main.tile[num244, num245 - 1].HasTile)
+                        if (!Main.tile[num244, num245 - 1].HasTile && !Main.tile[num244, num245].IsHalfBlock && Main.tile[num244, num245].Slope == SlopeType.Solid)
                         {
-                            WorldGen.PlaceTile(num244, num245 - 1, ModContent.TileType<ContagionShortGrass>(), true, style: WorldGen.genRand.Next(23));
+                            Terraria.Tile t = Main.tile[num244, num245 - 1];
+                            t.TileType = (ushort)grass;
+                            t.HasTile = true;
+                            if (WorldGen.genRand.NextBool(50) || WorldGen.genRand.NextBool(40))
+                            {
+                                t.TileFrameX = 8 * 18;
+                            }
+                            else if (WorldGen.genRand.NextBool(35) || (Main.tile[num244, num245 - 1].WallType == ModContent.WallType<ContagionGrassWall>()))
+                            {
+                                int style = WorldGen.genRand.NextFromList(6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22);
+                                t.TileFrameX = (short)(style * 18);
+                            }
+                            else
+                            {
+                                t.TileFrameX = (short)(WorldGen.genRand.Next(6) * 18);
+                            }
                         }
                     }
                 }
