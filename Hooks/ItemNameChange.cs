fb4e6b3617d5427b78cd59a02813ae1d47e373ba
blahblahbal <jabawakijadehawk@gmail.com>
2024-03-01 13:06:38 -0800

    whops
    did an oopsie

--- Hooks/ItemNameChange.cs
+++ Hooks/ItemNameChange.cs
@@ -10,7 +10,6 @@ internal class ItemNameChange : ModHook
 {
     protected override void Apply()
     {
-        //On_Lang.GetItemNameValue += On_Lang_GetItemNameValue;
         On_Lang.GetItemName += On_Lang_GetItemName;
     }
 
@@ -51,43 +50,6 @@ internal class ItemNameChange : ModHook
             if (id == ItemID.BloodMoonStarter)
                 return Language.GetText("Mods.Avalon.VanillaItemRenames.BloodyTear");
         }
-        return orig.Invoke(id);
-    }
-
-    private string On_Lang_GetItemNameValue(On_Lang.orig_GetItemNameValue orig, int id)
-    {
-        if (id == ItemID.DarkBlueSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Mushrooms");
-        if (id == ItemID.BlueSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Hallow");
-        if (id == ItemID.GreenSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Purity");
-        if (id == ItemID.PurpleSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Corruption");
-        if (id == ItemID.RedSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Crimson");
-        if (id == ItemID.SandSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Desert");
-        if (id == ItemID.SnowSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Snow");
-        if (id == ItemID.DirtSolution)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Solutions.Forest");
-        if (id == ItemID.FieryGreatsword)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.Volcano");
-        if (id == ItemID.PurpleMucos)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.PurpleMucos");
-        if (id == ItemID.CoinGun)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.CoinGun");
-        if (id == ItemID.FrostsparkBoots)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.FrostsparkBoots");
-        if (id == ItemID.CrimsonPlanterBox)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.CrimsonPlanterBox");
-        if (id == ItemID.HandOfCreation)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.HandOfCreation");
-        if (id == ItemID.HighTestFishingLine)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.FishingLine");
-        if (id == ItemID.BloodMoonStarter)
-            return Language.GetTextValue("Mods.Avalon.VanillaItemRenames.BloodyTear");
         return orig.Invoke(id);
     }
 }
