dfb8500b1bd1f0f3bd28e991e80ffbc889610c03
TerrorPenguin <terra_penguin@live.com>
2024-03-02 14:43:31 +1000

    corrected drop quantities from contagion crates

--- Items/Consumables/ContagionCrate.cs
+++ Items/Consumables/ContagionCrate.cs
@@ -49,58 +49,58 @@ public class ContagionCrate : ModItem
 
         // Drop pre-hm ores, with the addition of the Avalon ones
         IItemDropRule[] oreTypes = new IItemDropRule[] {
-            ItemDropRule.Common(ItemID.CopperOre, 1, 20, 36),
-            ItemDropRule.Common(ItemID.TinOre, 1, 20, 36),
-            ItemDropRule.Common(ModContent.ItemType<Material.Ores.BronzeOre>(), 1, 20, 36),
-            ItemDropRule.Common(ItemID.IronOre, 1, 20, 36),
-            ItemDropRule.Common(ItemID.LeadOre, 1, 20, 36),
-            ItemDropRule.Common(ModContent.ItemType<Material.Ores.NickelOre>(), 1, 20, 36),
-            ItemDropRule.Common(ItemID.SilverOre, 1, 20, 36),
-            ItemDropRule.Common(ItemID.TungstenOre, 1, 20, 36),
-            ItemDropRule.Common(ModContent.ItemType<Material.Ores.ZincOre>(), 1, 20, 36),
-            ItemDropRule.Common(ItemID.GoldOre, 1, 20, 36),
-            ItemDropRule.Common(ItemID.PlatinumOre, 1, 20, 36),
-            ItemDropRule.Common(ModContent.ItemType<Material.Ores.BismuthOre>(), 1, 20, 36),
+            ItemDropRule.Common(ItemID.CopperOre, 1, 20, 35),
+            ItemDropRule.Common(ItemID.TinOre, 1, 20, 35),
+            ItemDropRule.Common(ModContent.ItemType<Material.Ores.BronzeOre>(), 1, 20, 35),
+            ItemDropRule.Common(ItemID.IronOre, 1, 20, 35),
+            ItemDropRule.Common(ItemID.LeadOre, 1, 20, 35),
+            ItemDropRule.Common(ModContent.ItemType<Material.Ores.NickelOre>(), 1, 20, 35),
+            ItemDropRule.Common(ItemID.SilverOre, 1, 20, 35),
+            ItemDropRule.Common(ItemID.TungstenOre, 1, 20, 35),
+            ItemDropRule.Common(ModContent.ItemType<Material.Ores.ZincOre>(), 1, 20, 35),
+            ItemDropRule.Common(ItemID.GoldOre, 1, 20, 35),
+            ItemDropRule.Common(ItemID.PlatinumOre, 1, 20, 35),
+            ItemDropRule.Common(ModContent.ItemType<Material.Ores.BismuthOre>(), 1, 20, 35),
         };
         itemLoot.Add(new OneFromRulesRule(7, oreTypes));
 
         // Drop pre-hm bars (except copper/tin), with the addition of the Avalon ones
         IItemDropRule[] oreBars = new IItemDropRule[] {
-            ItemDropRule.Common(ItemID.IronBar, 1, 10, 21),
-            ItemDropRule.Common(ItemID.LeadBar, 1, 10, 21),
-            ItemDropRule.Common(ModContent.ItemType<Material.Bars.NickelBar>(), 1, 10, 21),
-            ItemDropRule.Common(ItemID.SilverBar, 1, 10, 21),
-            ItemDropRule.Common(ItemID.TungstenBar, 1, 10, 21),
-            ItemDropRule.Common(ModContent.ItemType<Material.Bars.ZincBar>(), 1, 10, 21),
-            ItemDropRule.Common(ItemID.GoldBar, 1, 10, 21),
-            ItemDropRule.Common(ItemID.PlatinumBar, 1, 10, 21),
-            ItemDropRule.Common(ModContent.ItemType<Material.Bars.BismuthBar>(), 1, 10, 21),
+            ItemDropRule.Common(ItemID.IronBar, 1, 6, 16),
+            ItemDropRule.Common(ItemID.LeadBar, 1, 6, 16),
+            ItemDropRule.Common(ModContent.ItemType<Material.Bars.NickelBar>(), 1, 6, 16),
+            ItemDropRule.Common(ItemID.SilverBar, 1, 6, 16),
+            ItemDropRule.Common(ItemID.TungstenBar, 1, 6, 16),
+            ItemDropRule.Common(ModContent.ItemType<Material.Bars.ZincBar>(), 1, 6, 16),
+            ItemDropRule.Common(ItemID.GoldBar, 1, 6, 16),
+            ItemDropRule.Common(ItemID.PlatinumBar, 1, 6, 16),
+            ItemDropRule.Common(ModContent.ItemType<Material.Bars.BismuthBar>(), 1, 6, 16),
         };
         itemLoot.Add(new OneFromRulesRule(4, oreBars));
 
         // Drop an "exploration utility" potion, with the addition of the Avalon ones
         IItemDropRule[] explorationPotions = new IItemDropRule[] {
-            ItemDropRule.Common(ItemID.ObsidianSkinPotion, 1, 2, 5),
-            ItemDropRule.Common(ItemID.SpelunkerPotion, 1, 2, 5),
-            ItemDropRule.Common(ItemID.HunterPotion, 1, 2, 5),
-            ItemDropRule.Common(ItemID.GravitationPotion, 1, 2, 5),
-            ItemDropRule.Common(ItemID.MiningPotion, 1, 2, 5),
-            ItemDropRule.Common(ItemID.HeartreachPotion, 1, 2, 5),
+            ItemDropRule.Common(ItemID.ObsidianSkinPotion, 1, 2, 4),
+            ItemDropRule.Common(ItemID.SpelunkerPotion, 1, 2, 4),
+            ItemDropRule.Common(ItemID.HunterPotion, 1, 2, 4),
+            ItemDropRule.Common(ItemID.GravitationPotion, 1, 2, 4),
+            ItemDropRule.Common(ItemID.MiningPotion, 1, 2, 4),
+            ItemDropRule.Common(ItemID.HeartreachPotion, 1, 2, 4),
         };
         itemLoot.Add(new OneFromRulesRule(4, explorationPotions));
 
         // Drop (pre-hm) resource potion
         IItemDropRule[] resourcePotions = new IItemDropRule[] {
-            ItemDropRule.Common(ItemID.HealingPotion, 1, 5, 18),
-            ItemDropRule.Common(ItemID.ManaPotion, 1, 5, 18),
-            ItemDropRule.Common(ModContent.ItemType<Potions.Other.StaminaPotion>(), 1, 5, 18),
+            ItemDropRule.Common(ItemID.HealingPotion, 1, 5, 17),
+            ItemDropRule.Common(ItemID.ManaPotion, 1, 5, 17),
+            ItemDropRule.Common(ModContent.ItemType<Potions.Other.StaminaPotion>(), 1, 5, 17),
         };
         itemLoot.Add(new OneFromRulesRule(2, resourcePotions));
 
         // Drop (high-end) bait
         IItemDropRule[] highendBait = new IItemDropRule[] {
-            ItemDropRule.Common(ItemID.JourneymanBait, 1, 2, 7),
-            ItemDropRule.Common(ItemID.MasterBait, 1, 2, 7),
+            ItemDropRule.Common(ItemID.JourneymanBait, 1, 2, 6),
+            ItemDropRule.Common(ItemID.MasterBait, 1, 2, 6),
         };
         itemLoot.Add(new OneFromRulesRule(2, highendBait));
     }
