99b722bf8d7ffa63f5d9d8c784adb4cf6e35c691
TerrorPenguin <terra_penguin@live.com>
2024-04-02 19:35:52 +1000

    lion says it works, don't wanna test, so it works

--- Tiles/GemTrees/TourmalineTree.cs
+++ Tiles/GemTrees/TourmalineTree.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using static Terraria.WorldGen;
 using Avalon.Items.Placeable.Tile;
 using Avalon.Items.Material.Ores;
+using Terraria.Graphics.Capture;
 
 namespace Avalon.Tiles.GemTrees;
 
@@ -272,7 +273,7 @@ public class TourmalineTree : ModTile
         Vector2 zero = Vector2.Zero;
         float num15 = 0.08f;
         float num16 = 0.06f;
-        int PositioningFix = 192; //Fix to the positioning to the Branches and Tops being 192 pixels to the top and left
+        int PositioningFix = CaptureManager.Instance.IsCapturing ? 0 : 192; //Fix to the positioning to the Branches and Tops being 192 pixels to the top and left
         int x = k;
         int y = l;
         Tile tile = Main.tile[x, y];
