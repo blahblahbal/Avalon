8a7406f1107c89cf242f2c15e87c98109c2314e6
TerrorPenguin <terra_penguin@live.com>
2024-04-04 13:06:11 +1000

    todo: fix dusts

--- Projectiles/SnotsandBall.cs
+++ Projectiles/SnotsandBall.cs
@@ -1,157 +1,44 @@
-using Microsoft.Xna.Framework;
-using System;
+using Avalon.Items.Placeable.Tile;
+using Avalon.Tiles.Contagion;
 using Terraria;
-using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Avalon.Projectiles;
 
-public class SnotsandBall : ModProjectile
+public abstract class SnotsandBall : ModProjectile
 {
+    public override string Texture => "Avalon/Projectiles/SnotsandBall";
     public override void SetStaticDefaults()
     {
         ProjectileID.Sets.FallingBlockDoesNotFallThroughPlatforms[Projectile.type] = true;
+        ProjectileID.Sets.ForcePlateDetection[Type] = true;
     }
-    public override void SetDefaults()
-    {
-        Projectile.knockBack = 6f;
-        Projectile.width = 10;
-        Projectile.height = 10;
-        Projectile.friendly = true;
-        Projectile.hostile = true;
-        Projectile.penetrate = -1;
-        Projectile.aiStyle = -1;
-    }
-
-    public override void AI()
-    {
-        if (Main.rand.NextBool(2))
-        {
-            int i = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<Dusts.SnotsandDust>(), 0f, Projectile.velocity.Y * 0.5f);
-            Main.dust[i].velocity.X *= 0.2f;
-        }
-        Projectile.velocity.Y += 0.41f;
-        Projectile.rotation += 0.1f;
-        if (Projectile.velocity.Y > 10f)
-        {
-            Projectile.velocity.Y = 10f;
-        }
-        base.AI();
-    }
-
-    public override void OnKill(int timeLeft)
-    {
-        Point p = Projectile.Center.ToTileCoordinates();
-        if (p.X >= 0 && p.X < Main.maxTilesX && p.Y >= 0 && p.Y < Main.maxTilesY)
-        {
-            Tile t = Main.tile[p.X, p.Y];
-            if (t.IsHalfBlock && Projectile.velocity.Y > 0f && Math.Abs(Projectile.velocity.Y) > Math.Abs(Projectile.velocity.X))
-            {
-                t = Main.tile[p.X, --p.Y];
-            }
-            if (Main.tileCut[t.TileType])
-            {
-                WorldGen.KillTile(p.X, p.Y);
-            }
-            if (!Main.tileSolid[t.TileType] || TileID.Sets.IsATreeTrunk[t.TileType])
-            {
-                Item.NewItem(WorldGen.GetItemSource_FromTileBreak(p.X, p.Y), p.X * 16, p.Y * 16, 16, 16, ModContent.ItemType<Items.Placeable.Tile.SnotsandBlock>());
-                SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
-            }
-            if (!t.HasTile && t.TileType != TileID.MinecartTrack)
-            {
-                Tile tBelow = Main.tile[p.X, p.Y + 1];
-                if (tBelow.Slope != SlopeType.Solid)
-                {
-                    tBelow.Slope = SlopeType.Solid;
-                }
-                if (tBelow.IsHalfBlock) tBelow.IsHalfBlock = false;
-                WorldGen.PlaceTile(p.X, p.Y, ModContent.TileType<Tiles.Contagion.Snotsand>(), forced: true);
-                WorldGen.SquareTileFrame(p.X, p.Y);
-            }
-        }
-    }
-
-    public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
-    {
-        fallThrough = !ProjectileID.Sets.FallingBlockDoesNotFallThroughPlatforms[Projectile.type];
-        return true;
-    }
-    public override bool? CanDamage() => Projectile.localAI[1] != -1f;
 }
 
-public class SnotsandSandgunProjectile : ModProjectile
+public class SnotsandBallFallingProjectile : SnotsandBall
 {
-    public override string Texture => ModContent.GetInstance<SnotsandBall>().Texture;
     public override void SetStaticDefaults()
     {
-        ProjectileID.Sets.FallingBlockDoesNotFallThroughPlatforms[Projectile.type] = true;
+        base.SetStaticDefaults();
+        ProjectileID.Sets.FallingBlockTileItem[Type] = new(ModContent.TileType<Snotsand>(), ModContent.ItemType<SnotsandBlock>());
     }
     public override void SetDefaults()
     {
-        Projectile.CloneDefaults(ProjectileID.EbonsandBallGun);
-        Projectile.aiStyle = -1;
+        Projectile.CloneDefaults(ProjectileID.CrimsandBallFalling);
     }
+}
 
-    public override void AI()
+public class SnotsandSandgunProjectile : SnotsandBall
+{
+    public override void SetStaticDefaults()
     {
-        if (Main.rand.NextBool(2))
-        {
-            int i = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, ModContent.DustType<Dusts.SnotsandDust>(), 0f, Projectile.velocity.Y * 0.5f);
-            Main.dust[i].velocity.X *= 0.2f;
-        }
-        Projectile.ai[1]++;
-        if (Projectile.ai[1] >= 60)
-        {
-            Projectile.ai[1] = 60;
-            Projectile.velocity.Y += 0.2f;
-        }
-        Projectile.rotation += 0.1f;
-        if (Projectile.velocity.Y > 10f)
-        {
-            Projectile.velocity.Y = 10f;
-        }
-        base.AI();
+        base.SetStaticDefaults();
+        ProjectileID.Sets.FallingBlockTileItem[Type] = new(ModContent.TileType<Snotsand>());
     }
-
-    public override void OnKill(int timeLeft)
+    public override void SetDefaults()
     {
-        Point p = Projectile.Center.ToTileCoordinates();
-        if (p.X >= 0 && p.X < Main.maxTilesX && p.Y >= 0 && p.Y < Main.maxTilesY)
-        {
-            Tile t = Main.tile[p.X, p.Y];
-            if (t.IsHalfBlock && Projectile.velocity.Y > 0f && Math.Abs(Projectile.velocity.Y) > Math.Abs(Projectile.velocity.X))
-            {
-                t = Main.tile[p.X, --p.Y];
-            }
-            if (Main.tileCut[t.TileType])
-            {
-                WorldGen.KillTile(p.X, p.Y);
-            }
-            if (!Main.tileSolid[t.TileType] || TileID.Sets.IsATreeTrunk[t.TileType])
-            {
-                Item.NewItem(WorldGen.GetItemSource_FromTileBreak(p.X, p.Y), p.X * 16, p.Y * 16, 16, 16, ModContent.ItemType<Items.Placeable.Tile.SnotsandBlock>());
-                SoundEngine.PlaySound(SoundID.Dig, Projectile.Center);
-            }
-            if (!t.HasTile && t.TileType != TileID.MinecartTrack)
-            {
-                Tile tBelow = Main.tile[p.X, p.Y + 1];
-                if (tBelow.Slope != SlopeType.Solid)
-                {
-                    tBelow.Slope = SlopeType.Solid;
-                }
-                if (tBelow.IsHalfBlock) tBelow.IsHalfBlock = false;
-                WorldGen.PlaceTile(p.X, p.Y, ModContent.TileType<Tiles.Contagion.Snotsand>(), forced: true);
-                WorldGen.SquareTileFrame(p.X, p.Y);
-            }
-        }
+        Projectile.CloneDefaults(ProjectileID.CrimsandBallGun);
+        AIType = ProjectileID.CrimsandBallGun;
     }
-
-    public override bool TileCollideStyle(ref int width, ref int height, ref bool fallThrough, ref Vector2 hitboxCenterFrac)
-    {
-        fallThrough = !ProjectileID.Sets.FallingBlockDoesNotFallThroughPlatforms[Projectile.type];
-        return true;
-    }
-    public override bool? CanDamage() => Projectile.localAI[1] != -1f;
 }
--- Reflection/WorldGenHelper.cs
+++ Reflection/WorldGenHelper.cs
@@ -8,6 +8,6 @@ public static class WorldGenHelper
     public delegate bool GrowMoreVinesDelegate(int x, int y);
     public static readonly GrowMoreVinesDelegate GrowMoreVines = Utilities.CacheStaticMethod<GrowMoreVinesDelegate>(typeof(WorldGen).GetMethod("GrowMoreVines", BindingFlags.Static | BindingFlags.NonPublic)!);
 
-    public delegate bool SpawnFallingBlockProjectileDelegate(int i, int j, Tile tileCache, Tile tileTopCache, Tile tileBottomCache, int type);
-    public static readonly SpawnFallingBlockProjectileDelegate SpawnFallingBlockProjectile = Utilities.CacheStaticMethod<SpawnFallingBlockProjectileDelegate>(typeof(WorldGen).GetMethod("SpawnFallingBlockProjectile", BindingFlags.Static | BindingFlags.NonPublic)!);
+    //public delegate bool SpawnFallingBlockProjectileDelegate(int i, int j, Tile tileCache, Tile tileTopCache, Tile tileBottomCache, int type);
+    //public static readonly SpawnFallingBlockProjectileDelegate SpawnFallingBlockProjectile = Utilities.CacheStaticMethod<SpawnFallingBlockProjectileDelegate>(typeof(WorldGen).GetMethod("SpawnFallingBlockProjectile", BindingFlags.Static | BindingFlags.NonPublic)!);
 }
