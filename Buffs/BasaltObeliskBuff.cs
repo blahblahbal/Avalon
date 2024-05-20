3367662d3928ca0f9b812bf9d21640d442a34642
TerrorPenguin <terra_penguin@live.com>
2024-05-21 08:24:36 +1000

    Update BasaltObeliskBuff.cs

--- Buffs/BasaltObeliskBuff.cs
+++ Buffs/BasaltObeliskBuff.cs
@@ -2,6 +2,7 @@ using Terraria;
 using Terraria.ModLoader;
 using Terraria.ID;
 using Avalon.Biomes;
+using Avalon.Common;
 
 namespace Avalon.Buffs;
 
@@ -14,19 +15,31 @@ public class BasaltObeliskBuff : ModBuff
 
     public override void Update(Player player, ref int buffIndex)
 	{
-		if (player.InModBiome(ModContent.GetInstance<BasaltObeliskBiome>()))
-		{
-			Main.buffNoTimeDisplay[Type] = true;
-		}
-		else
-		{
-			Main.buffNoTimeDisplay[Type] = false;
-		}
 		player.lavaMax += 420;
 		if (player.lavaTime < player.lavaMax && !player.lavaWet)
 		{
 			player.lavaTime++;
 		}
 		player.buffImmune[BuffID.OnFire] = true;
+	}
+}
+
+public class DisableBuffTimerDrawing : ModHook
+{
+	protected override void Apply()
+	{
+		On_Main.TryGetBuffTime += OnTryGetBuffTime;
+	}
+	private bool OnTryGetBuffTime(On_Main.orig_TryGetBuffTime orig, int buffSlotOnPlayer, out int buffTimeValue)
+	{
+		Player player = Main.LocalPlayer;
+		if (!player.InModBiome(ModContent.GetInstance<BasaltObeliskBiome>()) || player.buffType[buffSlotOnPlayer] != ModContent.BuffType<BasaltObeliskBuff>())
+		{
+			return orig.Invoke(buffSlotOnPlayer, out buffTimeValue);
+		}
+		else
+		{
+			return !orig.Invoke(buffSlotOnPlayer, out buffTimeValue);
+		}
 	}
 }
