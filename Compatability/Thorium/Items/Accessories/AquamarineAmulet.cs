f5f2a229711f4365f7d6ed220892aa4609c1d739
TerrorPenguin <terra_penguin@live.com>
2024-05-23 16:00:46 +1000

    thored ium

--- Compatability/Thorium/Items/Accessories/AquamarineAmulet.cs
+++ Compatability/Thorium/Items/Accessories/AquamarineAmulet.cs
@@ -7,6 +7,7 @@ using ThoriumMod;
 namespace Avalon.Compatability.Thorium.Items.Accessories;
 
 [ExtendsFromMod("ThoriumMod")]
+[AutoloadEquip(EquipType.Neck)]
 public class AquamarineAmulet : ModItem
 {
     public override bool IsLoadingEnabled(Mod mod)
