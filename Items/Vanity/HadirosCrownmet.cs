284db8c68586359ae2fbd5153cdd58d3aadc079f
TerrorPenguin <terra_penguin@live.com>
2024-06-13 20:47:54 +1000

    hadiros' set

---
+++ Items/Vanity/HadirosCrownmet.cs
@@ -0,0 +1,20 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Avalon.Items.Vanity;
+
+[AutoloadEquip(EquipType.Head)]
+class HadirosCrownmet : ModItem
+{
+    public override void SetDefaults()
+    {
+        Rectangle dims = this.GetDims();
+        Item.rare = ItemRarityID.Purple;
+        Item.width = dims.Width;
+        Item.vanity = true;
+        Item.value = Item.sellPrice(0, 5, 0, 0);
+        Item.height = dims.Height;
+    }
+}
