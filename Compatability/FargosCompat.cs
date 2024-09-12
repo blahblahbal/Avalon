cc0b3d6b4734d85392545923a280ccd47bb66d3c
blahblahbal <jabawakijadehawk@gmail.com>
2024-09-12 06:25:01 -0700

    fargo's compat
    errors if run from visual studio with fargo's enabled for some reason

---
+++ Compatability/FargosCompat.cs
@@ -0,0 +1,26 @@
+using Avalon.Tiles;
+using Avalon.Tiles.Contagion;
+using Avalon.Walls;
+using Terraria.ModLoader;
+
+namespace Avalon.Compatability;
+
+internal class FargosCompat : ModSystem
+{
+	public override void PostSetupContent()
+	{
+		//if (ExxoAvalonOrigins.Fargo == null)
+		//{
+		//	return;
+		//}
+		if (ModLoader.TryGetMod("Fargowiltas", out Mod fargos))
+		{
+			fargos.Call("AddEvilAltar", ModContent.TileType<IckyAltar>());
+			fargos.Call("AddIndestructibleTileType", ModContent.TileType<ImperviousBrick>());
+			fargos.Call("AddIndestructibleWallType", ModContent.WallType<ImperviousBrickWallUnsafe>());
+			fargos.Call("AddIndestructibleWallType", ModContent.WallType<ImperviousBrickWallBrownUnsafe>());
+			fargos.Call("AddIndestructibleWallType", ModContent.WallType<ImperviousBrickWallEctoUnsafe>());
+			fargos.Call("AddIndestructibleWallType", ModContent.WallType<ImperviousBrickWallWhiteUnsafe>());
+		}
+	}
+}
