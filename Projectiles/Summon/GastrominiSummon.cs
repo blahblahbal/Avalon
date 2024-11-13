1e81900c9b9d183f086c25488d53935604b6a7ab
TerrorPenguin <terra_penguin@live.com>
2024-11-13 10:00:39 +1000

    Update GastrominiSummon.cs
    no longer attacks target dummies
    now attempts to attack player's targeted npc (if it can't, just attacks nearest)

--- Projectiles/Summon/GastrominiSummon.cs
+++ Projectiles/Summon/GastrominiSummon.cs
@@ -55,52 +55,70 @@ public abstract class GastrominiSummon : ModProjectile
             Projectile.timeLeft = 2;
         }
 
-        var num820 = 0.05f;
-        for (var num821 = 0; num821 < 1000; num821++)
+        float idleAccel = 0.05f;
+        for (var k = 0; k < Main.maxProjectiles; k++)
         {
-            if (num821 != Projectile.whoAmI && Main.projectile[num821].active && Main.projectile[num821].owner == Projectile.owner && Math.Abs(Projectile.position.X - Main.projectile[num821].position.X) + Math.Abs(Projectile.position.Y - Main.projectile[num821].position.Y) < Projectile.width)
+            if (k != Projectile.whoAmI && Main.projectile[k].active && Main.projectile[k].owner == Projectile.owner && Math.Abs(Projectile.position.X - Main.projectile[k].position.X) + Math.Abs(Projectile.position.Y - Main.projectile[k].position.Y) < Projectile.width)
             {
-                if (Projectile.position.X < Main.projectile[num821].position.X)
+                if (Projectile.position.X < Main.projectile[k].position.X)
                 {
-                    Projectile.velocity.X = Projectile.velocity.X - num820;
+                    Projectile.velocity.X = Projectile.velocity.X - idleAccel;
                 }
                 else
                 {
-                    Projectile.velocity.X = Projectile.velocity.X + num820;
+                    Projectile.velocity.X = Projectile.velocity.X + idleAccel;
                 }
-                if (Projectile.position.Y < Main.projectile[num821].position.Y)
+                if (Projectile.position.Y < Main.projectile[k].position.Y)
                 {
-                    Projectile.velocity.Y = Projectile.velocity.Y - num820;
+                    Projectile.velocity.Y = Projectile.velocity.Y - idleAccel;
                 }
                 else
                 {
-                    Projectile.velocity.Y = Projectile.velocity.Y + num820;
+                    Projectile.velocity.Y = Projectile.velocity.Y + idleAccel;
                 }
             }
         }
-        var vector57 = Projectile.position;
-        var num822 = 400f;
-        var flag32 = false;
+        Vector2 targetPos = Projectile.position;
+        float targetDist = 400f;
+        bool target = false;
         if (Projectile.ai[0] != 1f)
         {
             Projectile.tileCollide = true;
         }
-        for (var num823 = 0; num823 < 200; num823++)
-        {
-            var nPC5 = Main.npc[num823];
-            if (nPC5.active && !nPC5.dontTakeDamage && !nPC5.friendly && nPC5.lifeMax > 5)
-            {
-                var num824 = Vector2.Distance(nPC5.Center, Projectile.Center);
-                if (num824 < num822 && Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, nPC5.position, nPC5.width, nPC5.height))
-                {
-                    num822 = num824;
-                    vector57 = nPC5.Center;
-                    flag32 = true;
-                }
-            }
-        }
+		bool cannotReachPlayerTarget = false;
+		if (player.HasMinionAttackTargetNPC)
+		{
+			NPC npc = Main.npc[player.MinionAttackTargetNPC];
+			if (Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height))
+			{
+				targetDist = Vector2.Distance(Projectile.Center, targetPos);
+				targetPos = npc.Center;
+				target = true;
+			}
+			else
+			{
+				cannotReachPlayerTarget = true;
+			}
+		}
+		if (!player.HasMinionAttackTargetNPC || cannotReachPlayerTarget)
+		{
+			for (int k = 0; k < Main.maxNPCs; k++)
+			{
+				NPC npc = Main.npc[k];
+				if (npc.CanBeChasedBy(this, false))
+				{
+					float distance = Vector2.Distance(npc.Center, Projectile.Center);
+					if ((distance < targetDist || !target) && Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, npc.position, npc.width, npc.height))
+					{
+						targetDist = distance;
+						targetPos = npc.Center;
+						target = true;
+					}
+				}
+			}
+		}
         var num825 = 500;
-        if (flag32)
+        if (target)
         {
             num825 = 1000;
         }
@@ -111,9 +129,9 @@ public abstract class GastrominiSummon : ModProjectile
             Projectile.tileCollide = false;
             Projectile.netUpdate = true;
         }
-        if (flag32 && Projectile.ai[0] == 0f)
+        if (target && Projectile.ai[0] == 0f)
         {
-            var vector58 = vector57 - Projectile.Center;
+            var vector58 = targetPos - Projectile.Center;
             var num826 = vector58.Length();
             vector58.Normalize();
             if (num826 > 200f)
@@ -212,14 +230,14 @@ public abstract class GastrominiSummon : ModProjectile
         {
             var scaleFactor7 = 8f;
             int num832 = ProjectileID.MiniRetinaLaser;
-            if (flag32 && Projectile.ai[1] == 0f)
+            if (target && Projectile.ai[1] == 0f)
             {
                 Projectile.ai[1] += 1f;
                 Projectile.localAI[0] = 1;
-                if (Main.myPlayer == Projectile.owner && Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, vector57, 0, 0))
+                if (Main.myPlayer == Projectile.owner && Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, targetPos, 0, 0))
                 {
-                    Projectile.spriteDirection = -(int)((vector57.X - Projectile.Center.X) / Math.Abs(vector57.X - Projectile.Center.X));
-                    var value24 = vector57 - Projectile.Center;
+                    Projectile.spriteDirection = -(int)((targetPos.X - Projectile.Center.X) / Math.Abs(targetPos.X - Projectile.Center.X));
+                    var value24 = targetPos - Projectile.Center;
                     value24.Normalize();
                     value24 *= scaleFactor7;
                     var num833 = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center.X, Projectile.Center.Y, value24.X, value24.Y, num832, (int)(Projectile.damage * 1.5f), 0f, Main.myPlayer, 0f, 0f);
