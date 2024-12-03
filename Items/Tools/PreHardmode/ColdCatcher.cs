c9452110cd7e89072f868f52229878ebde6f81b6
TerrorPenguin <terra_penguin@live.com>
2024-12-03 10:32:49 +1000

    changed cold catcher line colour
    for some reason, the rgb values need to be divided by 2 to be accurate... yeah, idk

--- Items/Tools/PreHardmode/ColdCatcher.cs
+++ Items/Tools/PreHardmode/ColdCatcher.cs
@@ -9,24 +9,29 @@ namespace Avalon.Items.Tools.PreHardmode;
 
 public class ColdCatcher : ModItem
 {
-    public override void SetDefaults()
-    {
-        Item.width = 24;
-        Item.height = 28;
-        Item.shootSpeed = 16.5f;
-        Item.rare = ItemRarityID.Blue;
-        Item.useTime = 8;
-        Item.fishingPole = 25;
-        Item.shoot = ModContent.ProjectileType<ColdCatcherBobber>();
-        Item.useStyle = ItemUseStyleID.Swing;
-        Item.value = Item.sellPrice(0, 2, 80);
-        Item.useAnimation = 8;
-    }
-    public override void AddRecipes()
-    {
-        CreateRecipe(1)
-            .AddIngredient(ModContent.ItemType<Material.Bars.BacciliteBar>(), 10)
-            .AddTile(TileID.Anvils)
-            .Register();
-    }
+	public override void SetDefaults()
+	{
+		Item.width = 24;
+		Item.height = 28;
+		Item.shootSpeed = 16.5f;
+		Item.rare = ItemRarityID.Blue;
+		Item.useTime = 8;
+		Item.fishingPole = 25;
+		Item.shoot = ModContent.ProjectileType<ColdCatcherBobber>();
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.value = Item.sellPrice(0, 2, 80);
+		Item.useAnimation = 8;
+	}
+	public override void AddRecipes()
+	{
+		CreateRecipe(1)
+			.AddIngredient(ModContent.ItemType<Material.Bars.BacciliteBar>(), 10)
+			.AddTile(TileID.Anvils)
+			.Register();
+	}
+	public override void ModifyFishingLine(Projectile bobber, ref Vector2 lineOriginOffset, ref Color lineColor)
+	{
+		lineOriginOffset = new Vector2(46, -33);
+		lineColor = new Color(191 / 2, 212 / 2, 52 / 2);
+	}
 }
--- Projectiles/ColdCatcherBobber.cs
+++ Projectiles/ColdCatcherBobber.cs
@@ -6,19 +6,13 @@ namespace Avalon.Projectiles;
 
 public class ColdCatcherBobber : ModProjectile
 {
-    public override void SetStaticDefaults()
-    {
-    }
+	public override void SetStaticDefaults()
+	{
+	}
 
-    public override void SetDefaults()
-    {
-        Projectile.CloneDefaults(ProjectileID.BobberWooden);
-        DrawOriginOffsetY = -8; // Adjusts the draw position
-    }
-
-    public override void ModifyFishingLine(ref Vector2 lineOriginOffset, ref Color lineColor)/* tModPorter Note: Removed. Use ModItem.ModifyFishingLine */
-    {
-        lineOriginOffset = new Vector2(46, -33);
-        lineColor = new Color(139, 143, 18);
-    }
+	public override void SetDefaults()
+	{
+		Projectile.CloneDefaults(ProjectileID.BobberWooden);
+		DrawOriginOffsetY = -8; // Adjusts the draw position
+	}
 }
