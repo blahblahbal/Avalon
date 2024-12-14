51cebecf66e0b1eb25b39a3612647438c7298a80
TerrorPenguin <terra_penguin@live.com>
2024-12-14 15:47:06 +1000

    coloured tileshine dust IL edit

--- Hooks/TileDrawingHooks.cs
+++ Hooks/TileDrawingHooks.cs
@@ -1,6 +1,8 @@
 using Avalon.Common;
 using Avalon.Systems;
 using Avalon.Tiles;
+using Avalon.Tiles.Furniture;
+using Avalon.Tiles.Ores;
 using Microsoft.Xna.Framework;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
@@ -18,6 +20,7 @@ namespace Avalon.Hooks
 		protected override void Apply()
 		{
 			IL_TileDrawing.DrawSingleTile += BetterDrawEffects;
+			IL_TileDrawing.DrawTiles_EmitParticles += TintTileSparkle;
 		}
 		private void BetterDrawEffects(ILContext il)
 		{
@@ -40,6 +43,54 @@ namespace Avalon.Hooks
 				{
 					Color color = TileGlowDrawing.ActuatedColor(new Color(255, WarmGemsparkBlock.G, 0, 255), Main.tile[i, j]);
 					drawdata.finalColor = color;
+				}
+			});
+		}
+		private void TintTileSparkle(ILContext il)
+		{
+			ILCursor c = new(il);
+			c.GotoNext(MoveType.After, i => i.MatchCall<Color>("get_White"), i => i.MatchStloc(55));
+			c.EmitLdarg(1); //x
+			c.EmitLdarg(2); //y
+			c.EmitLdarg(3); //tileCache
+			c.EmitLdarg(4); //typeCache
+			c.EmitLdloca(55); //ref newColor
+			c.EmitDelegate((int i, int j, Tile tileCache, int typeCache, ref Color tileShineColor) =>
+			{
+				// tileShineColor doesn't show clearly with desaturated colours, so make sure to modify whatever colour you're using so the highest value is 255 or close
+
+				// ores
+				if (typeCache == ModContent.TileType<BacciliteOre>())
+				{
+					tileShineColor = new Color(240, 255, 50, 255);
+				}
+				if (typeCache == ModContent.TileType<ShroomiteOre>())
+				{
+					tileShineColor = new Color(0, 200, 255, 255);
+				}
+				if (typeCache == ModContent.TileType<XanthophyteOre>())
+				{
+					tileShineColor = new Color(255, 255, 0, 255);
+				}
+
+				// gems
+				if (typeCache == ModContent.TileType<Tourmaline>() || (typeCache == ModContent.TileType<PlacedGems>() && Main.tile[i, j].TileFrameX / 18 == 3))
+				{
+					tileShineColor = new Color(0, 255, 255, 255);
+				}
+				if (typeCache == ModContent.TileType<Peridot>() || (typeCache == ModContent.TileType<PlacedGems>() && Main.tile[i, j].TileFrameX / 18 == 4))
+				{
+					tileShineColor = new Color(235, 255, 0, 255);
+				}
+				if (typeCache == ModContent.TileType<Zircon>() || (typeCache == ModContent.TileType<PlacedGems>() && Main.tile[i, j].TileFrameX / 18 == 5))
+				{
+					tileShineColor = new Color(255, 230, 120, 255);
+				}
+
+				// misc
+				if (typeCache == ModContent.TileType<PlacedStaminaCrystal>())
+				{
+					tileShineColor = new Color(0, 255, 0, 255);
 				}
 			});
 		}
