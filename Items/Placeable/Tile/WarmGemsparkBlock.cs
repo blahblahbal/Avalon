6c3adcda84a78ba6fa537ae4823d2dd0ba914088
TerrorPenguin <terra_penguin@live.com>
2025-01-07 13:06:53 +1000

    colour shift gemspark is now coloured properly
    it wasn't coloured on cursor when in the active hotbar slot before

--- Items/Placeable/Tile/WarmGemsparkBlock.cs
+++ Items/Placeable/Tile/WarmGemsparkBlock.cs
@@ -9,25 +9,25 @@ namespace Avalon.Items.Placeable.Tile;
 
 public class WarmGemsparkBlock : ModItem
 {
-    public override void SetStaticDefaults()
-    {
-        Item.ResearchUnlockCount = 100;
-    }
+	public override void SetStaticDefaults()
+	{
+		Item.ResearchUnlockCount = 100;
+	}
 
-    public override void SetDefaults()
-    {
-        Rectangle dims = this.GetDims();
-        Item.autoReuse = true;
-        Item.consumable = true;
-        Item.createTile = ModContent.TileType<Tiles.WarmGemsparkBlock>();
-        Item.width = dims.Width;
-        Item.useTurn = true;
-        Item.useTime = 10;
-        Item.useStyle = ItemUseStyleID.Swing;
-        Item.maxStack = 9999;
-        Item.useAnimation = 15;
-        Item.height = dims.Height;
-    }
+	public override void SetDefaults()
+	{
+		Rectangle dims = this.GetDims();
+		Item.autoReuse = true;
+		Item.consumable = true;
+		Item.createTile = ModContent.TileType<Tiles.WarmGemsparkBlock>();
+		Item.width = dims.Width;
+		Item.useTurn = true;
+		Item.useTime = 10;
+		Item.useStyle = ItemUseStyleID.Swing;
+		Item.maxStack = 9999;
+		Item.useAnimation = 15;
+		Item.height = dims.Height;
+	}
 
 	public override void AddRecipes()
 	{
@@ -39,19 +39,8 @@ public class WarmGemsparkBlock : ModItem
 			.AddTile(TileID.WorkBenches)
 			.Register();
 	}
-	public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor,
-                                            Color itemColor, Vector2 origin, float scale)
-    {
-        spriteBatch.Draw(TextureAssets.Item[Item.type].Value, position, frame,
-            new Color(255, Tiles.WarmGemsparkBlock.G, 0), 0f, origin, scale, SpriteEffects.None, 0f);
-        return false;
-    }
-
-    public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation,
-                                        ref float scale, int whoAmI)
-    {
-        spriteBatch.Draw(TextureAssets.Item[Item.type].Value, Item.position - Main.screenPosition, null,
-            new Color(255, Tiles.WarmGemsparkBlock.G, 0), 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-        return false;
-    }
+	public override Color? GetAlpha(Color lightColor)
+	{
+		return new Color(255, Tiles.WarmGemsparkBlock.G, 0);
+	}
 }
