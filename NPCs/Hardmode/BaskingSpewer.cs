94c3dd27dd830dc12fcff3fe9bfa524efeb64b6a
TerrorPenguin <terra_penguin@live.com>
2025-01-09 04:49:15 +1000

    Update BaskingSpewer.cs

--- NPCs/Hardmode/BaskingSpewer.cs
+++ NPCs/Hardmode/BaskingSpewer.cs
@@ -1,8 +1,10 @@
+using Avalon.Common;
 using Microsoft.Xna.Framework;
-using System;
+using Microsoft.Xna.Framework.Graphics;
+using MonoMod.Cil;
+using System.Reflection;
 using Terraria;
-using Terraria.Audio;
-using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.GameContent.Bestiary;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
@@ -11,270 +13,163 @@ using Terraria.ModLoader;
 
 namespace Avalon.NPCs.Hardmode;
 
+public class SandSharkHook : ModHook
+{
+	protected override void Apply()
+	{
+		IL_NPC.UpdateCollision += preventModdedSandsharkCollision;
+		On_NPC.ApplyTileCollision += SandsharkCollision;
+	}
+	private void preventModdedSandsharkCollision(ILContext il)
+	{
+		ILCursor c = new(il);
+		ILLabel IL_0175 = c.DefineLabel();
+		c.GotoNext(MoveType.After, i => i.MatchLdarg(0), i => i.MatchLdfld<NPC>("type"), i => i.MatchLdcI4(72), i => i.MatchBeq(out IL_0175));
+		c.EmitLdarg0();
+		c.EmitDelegate((NPC self) =>
+		{
+			return self.type == ModContent.NPCType<BaskingSpewer>();
+		});
+		c.EmitBrtrue(IL_0175);
+	}
+	private void SandsharkCollision(On_NPC.orig_ApplyTileCollision orig, NPC self, bool fall, Vector2 cPosition, int cWidth, int cHeight)
+	{
+		if (self.type == ModContent.NPCType<BaskingSpewer>())
+		{
+			typeof(NPC).GetMethod("Collision_MoveSandshark", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(self, new object[] { fall, cPosition, cWidth, cHeight });
+		}
+		else
+		{
+			orig.Invoke(self, fall, cPosition, cWidth, cHeight);
+		}
+	}
+}
 public class BaskingSpewer : ModNPC
 {
-    public override void SetStaticDefaults()
-    {
-        Main.npcFrameCount[Type] = 4;
-        NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.Confused] = true;
-        Data.Sets.NPC.Wicked[NPC.type] = true;
+	public override void SetStaticDefaults()
+	{
+		Main.npcFrameCount[Type] = 4;
+		NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.Confused] = true;
+		NPCID.Sets.TrailingMode[Type] = 6;
+		Data.Sets.NPC.Wicked[NPC.type] = true;
+
 		NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers()
 		{
 			// Influences how the NPC looks in the Bestiary
-			Position = new Vector2(34f, 19f),
-			PortraitPositionXOverride = 0,
-			PortraitPositionYOverride = 18
+			Position = new Vector2(35f, -3f),
+			PortraitPositionXOverride = 0
 		};
 		NPCID.Sets.NPCBestiaryDrawOffset.Add(Type, value);
-    }
-    public override void SetDefaults()
-    {
-        NPC.CloneDefaults(NPCID.SandShark);
-        NPC.aiStyle = -1;
-        NPC.damage = 67;
-        NPC.defense = 23;
-        NPC.lifeMax = 420;
-        NPC.HitSound = SoundID.NPCHit1;
-        NPC.DeathSound = SoundID.NPCDeath1;
-        NPC.value = 400f;
-        NPC.npcSlots = 0.5f;
-        NPC.noGravity = true;
-        NPC.behindTiles = true;
-        AIType = NPCID.SandsharkCorrupt;
-        AnimationType = NPCID.SandsharkCorrupt;
-        SpawnModBiomes = new int[] { ModContent.GetInstance<Biomes.ContagionDesert>().Type };
 	}
-    public override float SpawnChance(NPCSpawnInfo spawnInfo)
-    {
-        return Main.tile[spawnInfo.SpawnTileX, spawnInfo.SpawnTileY].HasTile && spawnInfo.Player.InModBiome<Biomes.ContagionDesert>() && Main.hardMode ? 0.2f : 0f;
-    }
-    public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
-    {
-        bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
-        {
-            new FlavorTextBestiaryInfoElement(Language.GetTextValue("Mods.Avalon.Bestiary.BaskingSpewer"))
-        });
-    }
-    public static bool SandTiles(int type)
-    {
-        return TileID.Sets.Conversion.Sand[type] || TileID.Sets.Conversion.Sandstone[type] || TileID.Sets.Conversion.HardenedSand[type];
-    }
-    public override void AI()
-    {
-        if (NPC.direction == 0)
-        {
-            NPC.TargetClosest();
-        }
-        bool flag11 = true;
-        Point pt = NPC.Center.ToTileCoordinates();
-        Tile tileSafely7 = Framing.GetTileSafely(pt);
-        flag11 = tileSafely7.HasUnactuatedTile && SandTiles(tileSafely7.TileType);
-        if (flag11)
-        {
-            NPC.noTileCollide = true;
-        }
-        flag11 |= NPC.wet;
-        bool flag12 = false;
-        NPC.TargetClosest(faceTarget: false);
-        Vector2 vector233 = NPC.targetRect.Center.ToVector2();
-        if (Main.player[NPC.target].velocity.Y > -0.1f && !Main.player[NPC.target].dead && NPC.Distance(vector233) > 150f)
-        {
-            flag12 = true;
-        }
-        if (NPC.localAI[0] == -1f && !flag11)
-        {
-            NPC.localAI[0] = 20f;
-        }
-        if (NPC.localAI[0] > 0f)
-        {
-            NPC.localAI[0]--;
-        }
-        if (flag11)
-        {
-            if (NPC.soundDelay == 0)
-            {
-                float num648 = NPC.Distance(vector233) / 40f;
-                if (num648 < 10f)
-                {
-                    num648 = 10f;
-                }
-                if (num648 > 20f)
-                {
-                    num648 = 20f;
-                }
-                NPC.soundDelay = (int)num648;
-                SoundEngine.PlaySound(SoundID.WormDig, NPC.Center);
-            }
-            float num649 = NPC.ai[1];
-            bool flag14 = false;
-            pt = (NPC.Center + new Vector2(0f, 24f)).ToTileCoordinates();
-            tileSafely7 = Framing.GetTileSafely(pt.X, pt.Y);
-            if (tileSafely7.HasUnactuatedTile && SandTiles(tileSafely7.TileType))
-            {
-                flag14 = true;
-            }
-            NPC.ai[1] = flag14.ToInt();
-            if (NPC.ai[2] < 30f)
-            {
-                NPC.ai[2]++;
-            }
-            if (flag12)
-            {
-                NPC.TargetClosest();
-                NPC.velocity.X += (float)NPC.direction * 0.15f;
-                NPC.velocity.Y += (float)NPC.directionY * 0.15f;
-                if (NPC.velocity.X > 5f)
-                {
-                    NPC.velocity.X = 5f;
-                }
-                if (NPC.velocity.X < -5f)
-                {
-                    NPC.velocity.X = -5f;
-                }
-                if (NPC.velocity.Y > 3f)
-                {
-                    NPC.velocity.Y = 3f;
-                }
-                if (NPC.velocity.Y < -3f)
-                {
-                    NPC.velocity.Y = -3f;
-                }
-                Vector2 center44 = NPC.Center;
-                Vector2 val103 = NPC.velocity.SafeNormalize(Vector2.Zero);
-                Vector2 val29 = NPC.Size;
-                Vector2 vec4 = center44 + val103 * val29.Length() / 2f + NPC.velocity;
-                pt = vec4.ToTileCoordinates();
-                tileSafely7 = Framing.GetTileSafely(pt);
-                bool flag15 = tileSafely7.HasUnactuatedTile && (TileID.Sets.Conversion.Sand[tileSafely7.TileType] || TileID.Sets.Conversion.Sandstone[tileSafely7.TileType] || TileID.Sets.Conversion.HardenedSand[tileSafely7.TileType]);
-                if (flag15)
-                {
-                    NPC.noTileCollide = true;
-                }
-                if (!flag15 && NPC.wet)
-                {
-                    flag15 = tileSafely7.LiquidAmount > 0;
-                }
-                int num650 = 400;
-                if (Main.remixWorld)
-                {
-                    num650 = 700;
-                }
-                if (!flag15 && Math.Sign(NPC.velocity.X) == NPC.direction && NPC.Distance(vector233) < (float)num650 && (NPC.ai[2] >= 30f || NPC.ai[2] < 0f))
-                {
-                    if (NPC.localAI[0] == 0f)
-                    {
-                        //SoundEngine.PlaySound(14, NPC.Center, 542);
-                        NPC.localAI[0] = -1f;
-                    }
-                    NPC.ai[2] = -30f;
-                    Vector2 vector234 = NPC.DirectionTo(vector233 + new Vector2(0f, -80f));
-                    NPC.velocity = vector234 * 12f;
-                }
-            }
-            else
-            {
-                if (NPC.collideX)
-                {
-                    NPC.velocity.X *= -1f;
-                    NPC.direction *= -1;
-                    NPC.netUpdate = true;
-                }
-                if (NPC.collideY)
-                {
-                    NPC.netUpdate = true;
-                    NPC.velocity.Y *= -1f;
-                    NPC.directionY = Math.Sign(NPC.velocity.Y);
-                    NPC.ai[0] = NPC.directionY;
-                }
-                float num651 = 6f;
-                NPC.velocity.X += (float)NPC.direction * 0.1f;
-                if (NPC.velocity.X < 0f - num651 || NPC.velocity.X > num651)
-                {
-                    NPC.velocity.X *= 0.95f;
-                }
-                if (flag14)
-                {
-                    NPC.ai[0] = -1f;
-                }
-                else
-                {
-                    NPC.ai[0] = 1f;
-                }
-                float num652 = 0.06f;
-                float num653 = 0.01f;
-                if (NPC.ai[0] == -1f)
-                {
-                    NPC.velocity.Y -= num653;
-                    if (NPC.velocity.Y < 0f - num652)
-                    {
-                        NPC.ai[0] = 1f;
-                    }
-                }
-                else
-                {
-                    NPC.velocity.Y += num653;
-                    if (NPC.velocity.Y > num652)
-                    {
-                        NPC.ai[0] = -1f;
-                    }
-                }
-                if (NPC.velocity.Y > 0.4f || NPC.velocity.Y < -0.4f)
-                {
-                    NPC.velocity.Y *= 0.95f;
-                }
-            }
-        }
-        else
-        {
-            if (NPC.velocity.Y == 0f)
-            {
-                if (flag12)
-                {
-                    NPC.TargetClosest();
-                }
-                float num654 = 1f;
-                NPC.velocity.X += (float)NPC.direction * 0.1f;
-                if (NPC.velocity.X < 0f - num654 || NPC.velocity.X > num654)
-                {
-                    NPC.velocity.X *= 0.95f;
-                }
-            }
-            NPC.velocity.Y += 0.3f;
-            if (NPC.velocity.Y > 10f)
-            {
-                NPC.velocity.Y = 10f;
-            }
-            NPC.ai[0] = 1f;
-        }
-        NPC.rotation = NPC.velocity.Y * (float)NPC.direction * 0.1f;
-        if (NPC.rotation < -0.2f)
-        {
-            NPC.rotation = -0.2f;
-        }
-        if (NPC.rotation > 0.2f)
-        {
-            NPC.rotation = 0.2f;
-        }
-    }
-    public override void HitEffect(NPC.HitInfo hit)
-    {
-        if (NPC.life > 0)
-        {
-            return;
-        }
-        else if (NPC.life <= 0 && Main.netMode != NetmodeID.Server)
-        {
-            Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerHead").Type, NPC.scale);
-            Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerBody").Type, NPC.scale);
-            Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerFin").Type, NPC.scale);
-            Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerFin2").Type, NPC.scale);
-        }
-    }
-    public override void ModifyNPCLoot(NPCLoot npcLoot)
-    {
-        npcLoot.Add(ItemDropRule.Common(ItemID.SharkFin, 8));
-        npcLoot.Add(ItemDropRule.Common(ItemID.Nachos, 33));
-        npcLoot.Add(ItemDropRule.Common(ItemID.DarkShard, 25));
-        npcLoot.Add(ItemDropRule.ByCondition(new Conditions.WindyEnoughForKiteDrops(), ItemID.KiteSandShark, 25));
-    }
+	public override void SetDefaults()
+	{
+		NPC.CloneDefaults(NPCID.SandShark);
+		NPC.aiStyle = NPCAIStyleID.SandShark;
+		NPC.damage = 67;
+		NPC.defense = 23;
+		NPC.lifeMax = 420;
+		NPC.HitSound = SoundID.NPCHit1;
+		NPC.DeathSound = SoundID.NPCDeath1;
+		NPC.value = 400f;
+		NPC.npcSlots = 0.5f;
+		NPC.noGravity = true;
+		NPC.behindTiles = true;
+		AIType = NPCID.SandsharkCorrupt;
+		AnimationType = NPCID.SandsharkCorrupt;
+		SpawnModBiomes = [ModContent.GetInstance<Biomes.ContagionDesert>().Type];
+	}
+	public override bool? CanFallThroughPlatforms()
+	{
+		return true;
+	}
+	public override float SpawnChance(NPCSpawnInfo spawnInfo)
+	{
+		return Main.tile[spawnInfo.SpawnTileX, spawnInfo.SpawnTileY].HasTile && spawnInfo.Player.InModBiome<Biomes.ContagionDesert>() && Main.hardMode ? 0.2f : 0f;
+	}
+	public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
+	{
+		bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[]
+		{
+			new FlavorTextBestiaryInfoElement(Language.GetTextValue("Mods.Avalon.Bestiary.BaskingSpewer"))
+		});
+	}
+	public override void HitEffect(NPC.HitInfo hit)
+	{
+		if (NPC.life > 0)
+		{
+			return;
+		}
+		else if (NPC.life <= 0 && Main.netMode != NetmodeID.Server)
+		{
+			Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerHead").Type, NPC.scale);
+			Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerBody").Type, NPC.scale);
+			Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerFin").Type, NPC.scale);
+			Gore.NewGore(NPC.GetSource_Death(), NPC.position, NPC.velocity, Mod.Find<ModGore>("SpewerFin2").Type, NPC.scale);
+		}
+	}
+	public override void ModifyNPCLoot(NPCLoot npcLoot)
+	{
+		npcLoot.Add(ItemDropRule.Common(ItemID.SharkFin, 8));
+		npcLoot.Add(ItemDropRule.Common(ItemID.Nachos, 33));
+		npcLoot.Add(ItemDropRule.Common(ItemID.DarkShard, 25));
+		npcLoot.Add(ItemDropRule.ByCondition(new Conditions.WindyEnoughForKiteDrops(), ItemID.KiteSandShark, 25));
+	}
+
+	public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+	{
+		Vector2 frameSize = new Vector2(NPC.frame.Width, NPC.frame.Height);
+		Vector2 halfSize = new(frameSize.X / 2, frameSize.Y / 2);
+
+		SpriteEffects spriteEffects = SpriteEffects.None;
+		if (NPC.spriteDirection == 1)
+		{
+			spriteEffects = SpriteEffects.FlipHorizontally;
+		}
+
+		int trailAmount = 6;
+		int trailIncrement = trailAmount / 2;
+		float trailColorMod = trailAmount * 2;
+
+		for (int i = 1; i < trailAmount; i += trailIncrement)
+		{
+			Color color = NPC.GetAlpha(drawColor);
+			color *= (trailAmount - i) / trailColorMod;
+			Vector2 posTrail = NPC.oldPos[i] + new Vector2(NPC.width, NPC.height) / 2f - screenPos;
+			posTrail -= frameSize * NPC.scale / 2f;
+			posTrail += halfSize * NPC.scale + new Vector2(0f, NPC.gfxOffY);
+			spriteBatch.Draw(TextureAssets.Npc[Type].Value, posTrail, NPC.frame, color, NPC.rotation, halfSize, NPC.scale, spriteEffects, 0f);
+		}
+		Vector2 pos = NPC.Center - screenPos;
+		pos -= frameSize * NPC.scale / 2f;
+		pos += halfSize * NPC.scale + new Vector2(0f, NPC.gfxOffY);
+		spriteBatch.Draw(TextureAssets.Npc[Type].Value, pos, NPC.frame, NPC.GetAlpha(drawColor), NPC.rotation, halfSize, NPC.scale, spriteEffects, 0f);
+		return false;
+	}
+	public override Color? GetAlpha(Color drawColor)
+	{
+		float aMod = (255 - NPC.alpha) / 255f;
+		int r = (int)(drawColor.R * aMod);
+		int g = (int)(drawColor.G * aMod);
+		int b = (int)(drawColor.B * aMod);
+		int a = drawColor.A - NPC.alpha;
+		if (r + g + b > 10 && r + g + b >= 60)
+		{
+			r *= 2;
+			g *= 2;
+			b *= 2;
+			if (r > 255)
+			{
+				r = 255;
+			}
+			if (g > 255)
+			{
+				g = 255;
+			}
+			if (b > 255)
+			{
+				b = 255;
+			}
+		}
+		return new Color(r, g, b, a);
+	}
 }
