32914c955834ec4ed40e6929d96996ecb6b6c5c6
TerrorPenguin <terra_penguin@live.com>
2025-04-21 19:57:02 +1000

    Update Beak.cs

--- Items/Material/Beak.cs
+++ Items/Material/Beak.cs
@@ -1,27 +1,18 @@
-using Microsoft.Xna.Framework;
-using Terraria.ID;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace Avalon.Items.Material;
 
 public class Beak : ModItem
 {
-    public override void SetStaticDefaults()
-    {
-        Item.ResearchUnlockCount = 25;
-    }
+	public override void SetStaticDefaults()
+	{
+		Item.ResearchUnlockCount = 25;
+	}
 
-    public override void SetDefaults()
-    {
-        Item.autoReuse = true;
-        Item.consumable = true;
-        //Item.createTile = ModContent.TileType<Tiles.Beak>();
-        Item.useTime = 10;
-        Item.useTurn = true;
-        Item.maxStack = 9999;
-        Item.value = 50;
-        Item.useStyle = ItemUseStyleID.Swing;
-        Item.useAnimation = 15;
-        Item.Size = new Vector2(16);
-    }
+	public override void SetDefaults()
+	{
+		Item.DefaultToMisc(22, 14);
+		Item.value = Item.sellPrice(copper: 2);
+	}
 }
