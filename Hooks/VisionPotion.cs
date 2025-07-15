1b58787996138d4efd30d96eb26b59e981135c13
TerrorPenguin <terra_penguin@live.com>
2025-07-15 12:43:11 +1000

    optimisations, slightest behavioural difference
    (the behavioural difference is it spawns dust row by row instead of column by column)

--- Hooks/VisionPotion.cs
+++ Hooks/VisionPotion.cs
@@ -1,64 +1,82 @@
 using Avalon.Common;
 using Avalon.Common.Players;
 using Microsoft.Xna.Framework;
-using System;
 using Terraria;
 using Terraria.GameContent.Drawing;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Avalon.Hooks
 {
-    internal class VisionPotion : ModHook
-    {
-        protected override void Apply()
-        {
-            On_TileDrawing.GetScreenDrawArea += On_TileDrawing_GetScreenDrawArea;
-        }
+	internal class VisionPotion : ModHook
+	{
+		protected override void Apply()
+		{
+			On_TileDrawing.GetScreenDrawArea += On_TileDrawing_GetScreenDrawArea;
+		}
 
-        private void On_TileDrawing_GetScreenDrawArea(On_TileDrawing.orig_GetScreenDrawArea orig, TileDrawing self, Vector2 screenPosition, Vector2 offSet, out int firstTileX, out int lastTileX, out int firstTileY, out int lastTileY)
-        {
-            orig(self, screenPosition, offSet, out firstTileX, out lastTileX, out firstTileY, out lastTileY);
-            for (int j = firstTileX - 2; j < lastTileX + 2; j++)
-            {
-                for (int i = firstTileY; i < lastTileY + 4; i++)
-                {
-                    Tile tile = Main.tile[j, i];
+		private void On_TileDrawing_GetScreenDrawArea(On_TileDrawing.orig_GetScreenDrawArea orig, TileDrawing self, Vector2 screenPosition, Vector2 offSet, out int firstTileX, out int lastTileX, out int firstTileY, out int lastTileY)
+		{
+			orig(self, screenPosition, offSet, out firstTileX, out lastTileX, out firstTileY, out lastTileY);
+			if (Main.player[Main.myPlayer].GetModPlayer<AvalonPlayer>().Vision)
+			{
+				firstTileX -= 2;
+				lastTileX += 2;
+				lastTileY += 4;
+				for (int i = firstTileY; i < lastTileY; i++)
+				{
+					if (i > Main.worldSurface - 50 && i <= Main.worldSurface)
+					{
+						for (int j = firstTileX; j < lastTileX; j++)
+						{
+							Tile tile = Main.tile[j, i];
 
-                    if (!tile.HasTile && Main.player[Main.myPlayer].GetModPlayer<AvalonPlayer>().Vision && i > Main.worldSurface - 50 && i <= Main.worldSurface)
-                    {
-                        float brightness = Lighting.Brightness(j, i);
+							if (!tile.HasTile)
+							{
+								int diff = -(i - (int)Main.worldSurface);
+								if (diff == 0)
+								{
+									diff = 1;
+								}
+								if (!Main.gamePaused && Main.rand.NextBool(300 + diff * 75))
+								{
+									float brightness = Lighting.Brightness(j, i);
+									if (brightness < 0.5f)
+									{
+										Dust d = Dust.NewDustDirect(new Vector2(j * 16, i * 16), 16, 16, ModContent.DustType<Dusts.VisionPotion>(), 0f, 0f, (int)(brightness * 255) * 2, default, 0.1f);
+										d.fadeIn = 1f;
+										d.velocity *= 0.1f;
+										d.noLight = true;
+										d.noGravity = true;
+									}
+								}
+							}
+						}
+					}
+					else if (i > Main.worldSurface && i < Main.maxTilesY - 200)
+					{
+						for (int j = firstTileX; j < lastTileX; j++)
+						{
+							Tile tile = Main.tile[j, i];
 
-                        int diff = -(i - (int)Main.worldSurface);
-
-                        if (diff == 0)
-                        {
-                            diff = 1;
-                        }
-                        if (!Main.gamePaused && Main.rand.NextBool((int)(300 + diff * 75)))
-                        {
-                            int num28 = Dust.NewDust(new Vector2(j * 16, i * 16), 16, 16, ModContent.DustType<Dusts.VisionPotion>(), 0f, 0f, (int)(brightness * 255) * 2, default, 0.1f);
-                            Main.dust[num28].fadeIn = 1f;
-                            Main.dust[num28].velocity *= 0.1f;
-                            Main.dust[num28].noLight = true;
-                            Main.dust[num28].noGravity = true;
-                        }
-                    }
-
-                    if (!tile.HasTile && Main.player[Main.myPlayer].GetModPlayer<AvalonPlayer>().Vision && i > Main.worldSurface && i < Main.maxTilesY - 200)
-                    {
-                        float brightness = Lighting.Brightness(j, i);
-                        if (!Main.gamePaused && Main.rand.NextBool(300))
-                        {
-                            int num28 = Dust.NewDust(new Vector2(j * 16, i * 16), 16, 16, ModContent.DustType<Dusts.VisionPotion>(), 0f, 0f, (int)(brightness * 255) * 2, default, 0.1f);
-                            Main.dust[num28].fadeIn = 1f;
-                            Main.dust[num28].velocity *= 0.1f;
-                            Main.dust[num28].noLight = true;
-                            Main.dust[num28].noGravity = true;
-                        }
-                    }
-                }
-            }
-        }
-    }
+							if (!tile.HasTile)
+							{
+								if (!Main.gamePaused && Main.rand.NextBool(300))
+								{
+									float brightness = Lighting.Brightness(j, i);
+									if (brightness < 0.5f)
+									{
+										Dust d = Dust.NewDustDirect(new Vector2(j * 16, i * 16), 16, 16, ModContent.DustType<Dusts.VisionPotion>(), 0f, 0f, (int)(brightness * 255) * 2, default, 0.1f);
+										d.fadeIn = 1f;
+										d.velocity *= 0.1f;
+										d.noLight = true;
+										d.noGravity = true;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
