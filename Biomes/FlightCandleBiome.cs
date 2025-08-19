e49865dba05080f101001665eac32d11107a63dc
TerrorPenguin <terra_penguin@live.com>
2025-08-19 22:04:06 +1000

    changed biomes without their own music to -1 from Main.curMusic

--- Biomes/FlightCandleBiome.cs
+++ Biomes/FlightCandleBiome.cs
@@ -8,7 +8,7 @@ public class FlightCandleBiome : ModBiome
 {
 	public override SceneEffectPriority Priority => SceneEffectPriority.None;
 
-	public override int Music => Main.curMusic;
+	public override int Music => -1;
 	public override string BestiaryIcon => base.BestiaryIcon;
 	public override string BackgroundPath => base.BackgroundPath;
 	public override string MapBackground => BackgroundPath;
--- Biomes/SilenceCandleBiome.cs
+++ Biomes/SilenceCandleBiome.cs
@@ -2,20 +2,18 @@
 using Terraria;
 using Terraria.ModLoader;
 
-namespace Avalon.Biomes
+namespace Avalon.Biomes;
+public class SilenceCandleBiome : ModBiome
 {
-	public class SilenceCandleBiome : ModBiome
+	public override SceneEffectPriority Priority => SceneEffectPriority.None;
+
+	public override int Music => -1;
+	public override string BestiaryIcon => base.BestiaryIcon;
+	public override string BackgroundPath => base.BackgroundPath;
+	public override string MapBackground => BackgroundPath;
+
+	public override bool IsBiomeActive(Player player)
 	{
-		public override SceneEffectPriority Priority => SceneEffectPriority.None;
-
-		public override int Music => Main.curMusic;
-		public override string BestiaryIcon => base.BestiaryIcon;
-		public override string BackgroundPath => base.BackgroundPath;
-		public override string MapBackground => BackgroundPath;
-
-		public override bool IsBiomeActive(Player player)
-		{
-			return ModContent.GetInstance<BiomeTileCounts>().SilenceCandleNearby;
-		}
+		return ModContent.GetInstance<BiomeTileCounts>().SilenceCandleNearby;
 	}
 }
