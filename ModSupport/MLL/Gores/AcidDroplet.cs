442612d6d66b8010ebf2ef15850170876345b670
TerrorPenguin <terra_penguin@live.com>
2025-08-21 00:53:22 +1000

    little fix on droplet acceleration

--- ModSupport/MLL/Gores/AcidDroplet.cs
+++ ModSupport/MLL/Gores/AcidDroplet.cs
@@ -8,7 +8,7 @@ public class AcidDroplet : LiquidDropletGoreBase
 	public override float? DissipatingFrameTimeMult => 1.25f;
 	public override float? FallingFrameTimeMult => 1.25f;
 	public override float? SplashFrameTimeMult => 1.5f;
-	public override float? FallingAccel => 1.9f;
+	public override float? FallingAccel => 0.19f;
 	public override Vector3? LightColor => new(0f, 1f, 0f);
 	public override bool? HasSound => false;
 }
--- ModSupport/MLL/Gores/BloodDroplet.cs
+++ ModSupport/MLL/Gores/BloodDroplet.cs
@@ -8,6 +8,6 @@ public class BloodDroplet : LiquidDropletGoreBase
 	public override float? DissipatingFrameTimeMult => 1.25f;
 	public override float? FallingFrameTimeMult => 1.25f;
 	public override float? SplashFrameTimeMult => 1.5f;
-	public override float? FallingAccel => 1.9f;
+	public override float? FallingAccel => 0.19f;
 	public override Vector3? LightColor => new(0f, 1f, 0f);
 }
