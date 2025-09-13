dfd032682e079d6d933717bc89f2f631bb0e736e
TerrorPenguin <terra_penguin@live.com>
2025-09-13 14:40:50 +1000

    ahaha...

--- Projectiles/Melee/TetanusChakram.cs
+++ Projectiles/Melee/TetanusChakram.cs
@@ -1,38 +1,42 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace Avalon.Projectiles.Melee
 {
-    public class TetanusChakram : ModProjectile
-    {
-        public override void SetDefaults()
-        {
-            Projectile.CloneDefaults(ProjectileID.ThornChakram);
+	public class TetanusChakram : ModProjectile
+	{
+		public override void SetDefaults()
+		{
+			Projectile.CloneDefaults(ProjectileID.ThornChakram);
 			Projectile.width = Projectile.height = 14;
-        }
+			Rectangle dims = this.GetDims();
+			DrawOffsetX = -(int)((dims.Width / 2) - (Projectile.Size.X / 2));
+			DrawOriginOffsetY = -(int)((dims.Width / 2) - (Projectile.Size.Y / 2));
+		}
 
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-        {
-            OnHitAnything();
-        }
-        public override void OnHitPlayer(Player target, Player.HurtInfo info)
-        {
-            OnHitAnything();
-        }
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			OnHitAnything();
+		}
+		public override void OnHitPlayer(Player target, Player.HurtInfo info)
+		{
+			OnHitAnything();
+		}
 
-        void OnHitAnything()
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                int Type = Main.rand.NextBool(5) ? DustID.Iron : DustID.Blood;
-                Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, Type);
-                if(d.type == DustID.Blood)
-                {
-                    d.alpha = 128;
-                }
-                d.noGravity = !Main.rand.NextBool(3);
-            }
-        }
-    }
+		void OnHitAnything()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				int Type = Main.rand.NextBool(5) ? DustID.Iron : DustID.Blood;
+				Dust d = Dust.NewDustDirect(Projectile.position, Projectile.width, Projectile.height, Type);
+				if (d.type == DustID.Blood)
+				{
+					d.alpha = 128;
+				}
+				d.noGravity = !Main.rand.NextBool(3);
+			}
+		}
+	}
 }
