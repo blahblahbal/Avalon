1dd51686ec165fcfcc15b84722ec324fef071b1b
TerrorPenguin <terra_penguin@live.com>
2025-11-10 02:10:13 +1000

    earthsplitter sprites updated

--- PlayerDrawLayers/EarthsplitterArmorTendrils.cs
+++ PlayerDrawLayers/EarthsplitterArmorTendrils.cs
@@ -8,27 +8,29 @@ namespace Avalon.PlayerDrawLayers;
 
 public class EarthsplitterArmorTendrils : PlayerDrawLayer
 {
-    public override bool IsHeadLayer => true;
+	public override bool IsHeadLayer => true;
 
-    public override Position GetDefaultPosition() => new AfterParent(Terraria.DataStructures.PlayerDrawLayers.BackAcc);
+	public override Position GetDefaultPosition() => new AfterParent(Terraria.DataStructures.PlayerDrawLayers.BackAcc);
 
-    public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => true;
+	public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => true;
 
-    protected override void Draw(ref PlayerDrawSet drawInfo)
-    {
-        if (drawInfo.shadow != 0f)
-        {
-            return;
-        }
-        Player p = drawInfo.drawPlayer;
-        if (p.body == EquipLoader.GetEquipSlot(ExxoAvalonOrigins.Mod, "EarthsplitterChestpiece", EquipType.Body))
-        {
-            Vector2 value6 = new Vector2(-6 * p.direction, 8);
-            Vector2 vec4 = drawInfo.Position - Main.screenPosition + drawInfo.drawPlayer.bodyPosition + new Vector2(drawInfo.drawPlayer.width / 2, drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height / 2) + new Vector2(0f, -4f) + value6;
-            vec4 = vec4.Floor();
-            DrawData item = new DrawData(Mod.Assets.Request<Texture2D>("Items/Armor/Hardmode/EarthsplitterChestpiece_Back").Value, vec4, drawInfo.drawPlayer.bodyFrame, drawInfo.colorArmorBody, drawInfo.drawPlayer.bodyRotation, drawInfo.bodyVect, 1f, drawInfo.playerEffect, 0);
-            item.shader = drawInfo.cBody;
-            drawInfo.DrawDataCache.Add(item);
-        }
-    }
+	protected override void Draw(ref PlayerDrawSet drawInfo)
+	{
+		if (drawInfo.shadow != 0f)
+		{
+			return;
+		}
+		Player p = drawInfo.drawPlayer;
+		if (p.body == EquipLoader.GetEquipSlot(ExxoAvalonOrigins.Mod, "EarthsplitterChestpiece", EquipType.Body))
+		{
+			Vector2 value6 = new(-4 * p.direction, 8);
+			Vector2 vec4 = drawInfo.Position - Main.screenPosition + drawInfo.drawPlayer.bodyPosition + new Vector2(drawInfo.drawPlayer.width / 2, drawInfo.drawPlayer.height - drawInfo.drawPlayer.bodyFrame.Height / 2) + new Vector2(0f, -4f) + value6;
+			vec4 = vec4.Floor();
+			DrawData item = new(Mod.Assets.Request<Texture2D>("Items/Armor/Hardmode/EarthsplitterChestpiece_Back").Value, vec4, drawInfo.drawPlayer.bodyFrame, drawInfo.colorArmorBody, drawInfo.drawPlayer.bodyRotation, drawInfo.bodyVect, 1f, drawInfo.playerEffect, 0)
+			{
+				shader = drawInfo.cBody
+			};
+			drawInfo.DrawDataCache.Add(item);
+		}
+	}
 }
