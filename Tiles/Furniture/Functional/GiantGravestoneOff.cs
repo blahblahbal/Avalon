3e364b4b6c7837b2bc8442ec060f4a3f0ca0166e
TerrorPenguin <terra_penguin@live.com>
2026-09-13 00:30:06 +1000

    two more fixes

--- Tiles/Furniture/Functional/GiantGravestoneOff.cs
+++ Tiles/Furniture/Functional/GiantGravestoneOff.cs
@@ -1,4 +1,3 @@
-using Avalon.Common.Players;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.Audio;
@@ -14,22 +13,22 @@ namespace Avalon.Tiles.Furniture.Functional;
 
 public class GiantGravestoneOff : ModTile
 {
-    public override void SetStaticDefaults()
-    {
-        Main.tileFrameImportant[Type] = true;
-        Main.tileNoAttach[Type] = true;
+	public override void SetStaticDefaults()
+	{
+		Main.tileFrameImportant[Type] = true;
+		Main.tileNoAttach[Type] = true;
 		TileID.Sets.HasOutlines[Type] = true;
 		// Placement
 		TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
-        TileObjectData.newTile.Origin = new Point16(0, 1);
-        TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16 };
-        TileObjectData.newTile.StyleHorizontal = true;
-        TileObjectData.newTile.LavaDeath = false;
-        TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
-        TileObjectData.addTile(Type);
-        AddMapEntry(new Color(127, 127, 127), Language.GetText("Giant Gravestone"));
+		TileObjectData.newTile.Origin = new Point16(0, 1);
+		TileObjectData.newTile.CoordinateHeights = [16, 16, 16];
+		TileObjectData.newTile.StyleHorizontal = true;
+		TileObjectData.newTile.LavaDeath = false;
+		TileObjectData.newTile.AnchorBottom = new AnchorData(AnchorType.SolidTile | AnchorType.SolidWithTop | AnchorType.SolidSide, TileObjectData.newTile.Width, 0);
+		TileObjectData.addTile(Type);
+		AddMapEntry(new Color(127, 127, 127), Language.GetText("Mods.Avalon.Tiles.GiantGravestone.MapEntry"));
 		RegisterItemDrop(ModContent.ItemType<Items.Placeable.Furniture.GiantGravestone>());
-    }
+	}
 	public override void MouseOver(int i, int j)
 	{
 		Player player = Main.LocalPlayer;
