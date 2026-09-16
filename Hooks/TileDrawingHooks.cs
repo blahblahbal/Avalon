1515c9ac120de7ef93921bd8acb19db5c586e636
TerrorPenguin <terra_penguin@live.com>
2026-09-17 05:27:52 +1000

    tileshine2 colour ig idk weird ass vanilla code tbh

--- Hooks/TileDrawingHooks.cs
+++ Hooks/TileDrawingHooks.cs
@@ -1,10 +1,12 @@
 using Avalon.Common;
 using Avalon.Systems;
 using Avalon.Tiles;
+using Avalon.Tiles.Contagion;
 using Avalon.Tiles.Furniture;
 using Avalon.Tiles.Ores;
 using Microsoft.Xna.Framework;
 using MonoMod.Cil;
+using System;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameContent.Drawing;
@@ -16,9 +18,79 @@ namespace Avalon.Hooks
 	{
 		protected override void Apply()
 		{
+			On_Main.shine_Color_int += On_Main_shine_Color_int;
+			On_Main.shine_refVector3_int += On_Main_shine_refVector3_int;
+
 			IL_TileDrawing.DrawSingleTile += BetterDrawEffects;
 			IL_TileDrawing.DrawTiles_EmitParticles += TintTileSparkle;
 		}
+		private Color On_Main_shine_Color_int(On_Main.orig_shine_Color_int orig, Color newColor, int type)
+		{
+			newColor = new Color(GetShineColor(newColor.ToVector3(), type));
+			return orig.Invoke(newColor, type);
+		}
+
+		private void On_Main_shine_refVector3_int(On_Main.orig_shine_refVector3_int orig, ref Vector3 newColor, int type)
+		{
+			newColor = GetShineColor(newColor, type);
+			orig.Invoke(ref newColor, type);
+		}
+
+		/// <summary>
+		/// Add tiles here to modify the light color<br></br>
+		/// Seems to only work if they set <see cref="Main.tileShine2"/> to <see cref="true"/>, though several vanilla tiles have colors set here and do not set it, so I might've missed smth
+		/// </summary>
+		/// <param name="color"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		private static Vector3 GetShineColor(Vector3 color, int type)
+		{
+			Vector3 temp = color;
+			//if (type == ModContent.TileType<Chunkstone>())
+			//{
+			//	color.X *= 0.84f;
+			//	color.Y *= 0.91f;
+			//	color.Z *= 0.74f;
+			//}
+			if (type == ModContent.TileType<ShroomiteOre>())
+			{
+				float num = 0.3f + Utils.Remap(Main.mouseTextColor, 190, 255, 185, 260) / 300f;
+				color.Y *= 1.5f * num;
+				color.Z *= 1.1f * num;
+			}
+			else if (type == ModContent.TileType<XanthophyteOre>())
+			{
+				float num = 0.3f + Main.mouseTextColor / 300f;
+				color.X *= 1.1f * num;
+				color.Y *= 1.5f * num;
+			}
+			else if (
+				type == ModContent.TileType<CoolGemsparkBlock>() ||
+				type == ModContent.TileType<PeridotGemspark>() ||
+				type == ModContent.TileType<TourmalineGemspark>() ||
+				type == ModContent.TileType<WarmGemsparkBlock>() ||
+				type == ModContent.TileType<ZirconGemspark>()
+				)
+			{
+				color.X += 0.39f;
+				color.Y += 0.39f;
+				color.Z += 0.39f;
+			}
+			else if (type == ModContent.TileType<PlacedGems>())
+			{
+				color.X *= 1.5f;
+				color.Y *= 1.5f;
+				color.Z *= 1.5f;
+			}
+			if (Main.tileShine2[type] && color != temp) // vanilla multiplies tileShine2 tiles by 1.6 by default UNLESS they receive a custom colour, so negate that by dividing
+			{
+				color.X /= 1.6f;
+				color.Y /= 1.6f;
+				color.Z /= 1.6f;
+			}
+			return color;
+		}
+
 		private void BetterDrawEffects(ILContext il)
 		{
 			ILCursor c = new(il);
